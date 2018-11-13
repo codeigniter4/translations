152a61e6c5e3ea0f3e9fded78d6598225f108243
amilcar <amilcar.muratet@gmail.com>
2018-11-13 16:07:42 +0100

    add Files.php translation

--- Language/fr/Files.php
+++ Language/fr/Files.php
@@ -7,15 +7,14 @@
  * @copyright  2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
- * @since      Version 3.0.0
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
  */
 
 return [
-   'fileNotFound'    => 'File not found: {0}',
-   'cannotMove'      => 'Could not move file {0} to {1} ({2})',
-   'invalidFilename' => 'Target filename missing or invalid: {0}',
-   'cannotCopy'      => 'Could not copy to {0} - make sure the folder is writeable',
+   'fileNotFound'    => 'Fichier non trouvé : {0}',
+   'cannotMove'      => 'Impossible de déplacer le fichier {0} vers {1} ({2})',
+   'cannotCopy'      => 'Impossible de copier vers {0} - vérifiez que votre répertoire accessible en écriture',
 ];
