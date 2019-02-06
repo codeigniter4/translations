dd573af44f27d4759fbca50197358c766e7e98d5
Camilo Caquimbo Tabares <33238233+camilocaquimbo@users.noreply.github.com>
2019-02-05 22:20:14 -0500

    Update Session.php
    Typo

--- Language/es/Session.php
+++ Language/es/Session.php
@@ -15,7 +15,7 @@
 return [
 	'missingDatabaseTable'   => '`sessionSavePath` debe tener un nombre de tabla para que funcione el manejador de sesión de la base de datos.',
 	'invalidSavePath'        => "Sesión: La ruta de guardado configurada '{0}' no es un directorio, no existe o no puede ser creada.",
-	'writeProtectedSavePath' => "Sesión: La ruta de guardado configurada '{0}' no es escribile por el proceso de PHP.",
+	'writeProtectedSavePath' => "Sesión: La ruta de guardado configurada '{0}' no es escribible por el proceso de PHP.",
 	'emptySavePath'          => 'Sesión: No se ha configurado una ruta de guardado.',
 	'invalidSavePathFormat'  => 'Sesión: Formato de la ruta de guardado Redis inválida: {0}',
 ];
