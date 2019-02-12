a9b0ff0cfda6c642a5d543547030239707aab80f
Stefan Bauer <sba@gmx.ch>
2019-02-12 16:22:54 +0100

    code style

--- Language/de/View.php
+++ Language/de/View.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * View language strings.
  *
  * @package    CodeIgniter
- * @author     Stefan Bauer
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
  * @since      Version 3.0.0
