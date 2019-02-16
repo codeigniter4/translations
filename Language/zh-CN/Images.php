dc6df429ec50766ec761602f4d495d190c59a0be
HaoLiang <haoliang@outlook.jp>
2019-02-16 10:48:13 +0800

    Update chinese language v1.0.0

---
+++ Language/zh-CN/Images.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Image language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'sourceImageRequired'    => '必须制定原始图片。',
+   'gdRequired'             => 'GD 图像类需要使用此功能。',
+   'gdRequiredForProps'     => '服务器必须支持 GD 图像类才能获取图片信息。',
+   'gifNotSupported'        => '因为授权限制，通常不支持 GIF 图像。需要使用 JPG 或 PNG 图片。',
+   'jpgNotSupported'        => '不支持 JPG 图片。',
+   'pngNotSupported'        => '不支持 PNG 图片。',
+   'unsupportedImagecreate' => '服务器不支持处理此图片所需的 GD 库。',
+   'jpgOrPngRequired'       => '调整图片大小仅适用于 PNG 或 JPG 图片。',
+   'rotateUnsupported'      => '服务器似乎不支持图片旋转。',
+   'libPathInvalid'         => '图片库路径不正确，请设置正确的路径。{0, string)',
+   'imageProcessFailed'     => '图片处理失败，请确定服务器是否支持已选的协议，以及图片处理库的路径是否正确。',
+   'rotationAngleRequired'  => '宣传图片操作需要设置旋转角度。',
+   'invalidPath'            => '图片路径不正确。',
+   'copyFailed'             => '图片复制失败。',
+   'missingFont'            => '无法找到要使用的字体。',
+   'saveFailed'             => '无法保存图片，请确认图片和文件目录是可写的。',
+   'invalidDirection'       => '图片旋转方向只能是 `vertical` 或 `horizontal`，现在是: {0}',
+   'exifNotSupported'       => 'PHP 环境不支持读取图片的 EXIF 数据。',
+];
---
+++ Language/zh-CN/Migrations.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Migration language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+	// Migration Runner
+   'missingTable'      => '必须设置迁移表。',
+   'invalidType'       => '指定的迁移类型编号无效: {0}',
+   'disabled'          => '迁移状态为禁用或不正确，请确认 app/Config/Migrations 文件的 $enabled 值是否为 true。',
+   'notFound'          => '找不到指定的迁移文件：',
+   'empty'             => '没有指定的迁移文件。',
+   'gap'               => '版本号附近的迁移序列存在差异: ',
+   'classNotFound'     => '找不到指定的迁移类 "%s" 。',
+   'missingMethod'     => '迁移类缺少 "%s" 方法。',
+
+	// Migration Command
+   'migHelpLatest'     => "\t\t数据库将迁移至最新的可用迁移。",
+   'migHelpCurrent'    => "\t\t数据库将迁移至配置文件中 'current' 的版本。",
+   'migHelpVersion'    => "\t数据库将迁移至 {v} 版本。",
+   'migHelpRollback'   => "\t运行所有迁移 'down'方法 至 0 版本。",
+   'migHelpRefresh'    => "\t\t卸载且重新运行所有迁移，重置数据库。",
+   'migHelpSeed'       => "\t运行填充程序，名称：[name]",
+   'migCreate'         => "\t创建一个新的迁移，名称：[name]",
+   'nameMigration'     => '命名迁移文件。',
+   'badCreateName'     => '你必须提供迁移文件名。',
+   'writeError'        => '尝试创建文件时出错。',
+   'migNumberError'    => '迁移号必须为3位数字且中间不能有任何空格。',
+
+   'toLatest'          => '迁移到最新版本...',
+   'migInvalidVersion' => '提供的版本号无效。',
+   'toVersionPH'       => '迁移至 %s...',
+   'toVersion'         => '迁移至当前（current）版本...',
+   'rollingBack'       => '回滚所有迁移...',
+   'noneFound'         => '没有发现任何迁移。',
+   'on'                => '迁移: ',
+   'migSeeder'         => '填充（seeder）名',
+   'migMissingSeeder'  => '必须提供一个填充（seeder）名',
+   'removed'           => '回滚操作: ',
+   'added'             => '执行操作: ',
+
+   'version'           => '版本',
+   'filename'          => '文件名',
+];
