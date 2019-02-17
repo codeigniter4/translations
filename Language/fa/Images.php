6229f3013c585cc627858e7a47454e4079a76362
Mohammad Naji <smhnaji@gmail.com>
2019-02-18 00:13:23 +0330

    translate Images file

---
+++ Language/fa/Images.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Image language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+return [
+   'sourceImageRequired'    => 'لطفا در تنظیمات یک عکس مرجع انتخاب کنید.',
+   'gdRequired'             => 'کتابخانه‌ی GD برای استفاده از این قابلیت نیاز است.',
+   'gdRequiredForProps'     => 'برای تشخیص خصوصیات عکس، سرور شما باید از کتابخانه‌ی عکس GD پشتیبانی کند.',
+   'gifNotSupported'        => 'معمولا تصاویر GIF به دلایل محدودیت‌های لایسنس پشتیبانی نمی‌شوند. به جای آن می‌توانید از فرمت‌های JPG یا PNG استفاده کنید.',
+   'jpgNotSupported'        => 'تصاویر JPG پشتیبانی نمی‌شونند.',
+   'pngNotSupported'        => 'تصاویر PNG پشتیبانی نمی‌شونند.',
+   'unsupportedImagecreate' => 'برای پردازش این نوع تصاویر، سرور شما باید از GD پشتیبانی کند.',
+   'jpgOrPngRequired'       => 'تنظیمان انتابی شما برای تغییر اندازه‌ی عکس فقط برای فایل‌های JPEG و PNG قابل انجام است.',
+   'rotateUnsupported'      => 'بر اساس تنظیمات سرور شما، امکان چرخش تصویر وجود ندارد.',
+   'libPathInvalid'         => 'نشانی کتابخانه‌ی عکس شما صحیح نیست. لطفا در تنظیمات، مسیر درست را انتخاب کنید. {0, string)',
+   'imageProcessFailed'     => 'پردازش تصویر با مشکل روبرو شد. لطفا مطمئن شوید که سرور تنیمات انتخابی شما را پشتیبانی می‌کند و مسیر کتابخانه‌ی عکس صحیح است.',
+   'rotationAngleRequired'  => 'برای چرخش تصویر، لطفا زاویه‌ی چرخش را ارائه کنید.',
+   'invalidPath'            => 'نشانی تصویر انتخابی شما صحیح نیست.',
+   'copyFailed'             => 'کپی کردن تصویر با مشکل روبرو شد.',
+   'missingFont'            => 'برای استفاده، لطفا یک فونت انتخاب کنید.',
+   'saveFailed'             => 'مشکل در ذخیره کردن تصویر. لطفا مطمئن شوید که فایل و شاخه‌ی مورد نظر قابل نوشتن هستند.',
+   'invalidDirection'       => 'تقارن فقط می‌تواند افقی (`vertical`) یا عمودی (`horizontal`)) باشد. مقدار ارائه شده: {0}',
+   'exifNotSupported'       => 'در نسخه‌ی PHP نصب شده‌ی شما، EXIF پشتیبانی نمی‌شود.',
+];
