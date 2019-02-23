b6bbeace984be1d79b565aa1d72f041de008cbd4
Mohammad Naji <smhnaji@gmail.com>
2019-02-24 00:28:31 +0330

    Translate remaining files (if needed)

---
+++ Language/fa/Pager.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Pager language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'pageNavigation'         => 'ناوبری صفحه',
+   'first'                  => 'اولین',
+   'previous'               => 'قبلی',
+   'next'                   => 'بعدی',
+   'last'                   => 'آخرین',
+   'older'                  => 'قدیمی‌تر',
+   'newer'                  => 'جدیدتر',
+   'invalidTemplate'        => '{0} قالب صفحه‌ی معتبری نیست.',
+   'invalidPaginationGroup' => '{0} گروه ناوربری معتبری نیست.',
+];
