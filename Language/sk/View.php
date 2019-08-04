b4f8bd35ead111659e062d8a2f9a354650148558
xbotkaj <52993808+xbotkaj@users.noreply.github.com>
2019-08-04 07:51:05 +0200

    Remove "beta 4" in versions

--- Language/sk/View.php
+++ Language/sk/View.php
@@ -7,7 +7,7 @@
  * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
- * @since      Version 4.0.0 - beta 4
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
