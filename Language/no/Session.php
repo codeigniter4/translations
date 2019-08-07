ac07f45d47ed898036609be441611ddf7519279e
Kai Günter Brandt <kai.gunter.brandt@gmail.com>
2019-08-07 11:10:34 +0200

    First Norwegian translation

---
+++ Language/no/Session.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Session language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'missingDatabaseTable'   => 'I `sessionSavePath` må tabellnavnet spesifiseres slik at økthåndteringen fungerer via databasen.',
+   'invalidSavePath'        => "Hurtiglager: Den konfigurerte lagringsstien '{0}' er ikke en katalog, eksisterer ikke eller kan ikke opprettes.",
+   'writeProtectedSavePath' => "Hurtiglager: Den konfigurerte lagringsstien '{0}' kan ikke beskrives av PHP-prosessen.",
+   'emptySavePath'          => 'Hurtiglager: Ingen lagringsbane konfigurert.',
+   'invalidSavePathFormat'  => 'Hurtiglager: Ugyldig Redis lagringsbaneformat: {0}',
+];
