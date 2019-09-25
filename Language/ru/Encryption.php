75d1b86051637a376d595c36f352edbe3b15c1f1
Digital_Wolf <digital.wolf98@gmail.com>
2019-09-26 03:27:22 +0600

    Russian translation update for CI4.

---
+++ Language/ru/Encryption.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Encryption language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noDriverRequested'    => 'Требуется указать драйвер для шифрования.',
+   'noHandlerAvailable'   => 'Не удается найти доступный {0} обработчик шифрования.',
+   'unKnownHandler'       => "'{0}' не может быть настроен.",
+   'starterKeyNeeded'     => 'Для шифрования нужен секретный ключ.',
+   'authenticationFailed' => 'Не удалось выполнить проверку подлинности расшифровки.',
+   'encryptionFailed'     => 'Сбой шифрования.',
+];
