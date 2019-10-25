5c4f2d6e9b4889662c3dc790a40cbd70b33aefd1
Donatas Glodenis <dgvirtual@akl.lt>
2019-10-25 23:32:02 +0300

    trumpų failų pirminiai vertimai

---
+++ Language/lt/Entity.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * CLI language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return
+[
+   'tryingToAccessNonExistentProperty' => 'Bandoma pasiekti neegzistuojančią {1} savybę {0}',
+];
---
+++ Language/lt/Filters.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Filters language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noFilter'           => '{0} filtras turi turėti nustatytą slapyvardį.',
+   'incorrectInterface' => '{0} turi įgyvendinti CodeIgniter\Filters\FilterInterface.',
+];
