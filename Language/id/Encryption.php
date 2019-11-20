e10e8862f6d6a96d3c81adde496b83dd25d0e056
unknown <muh.ramadhan@email.com>
2019-11-20 18:32:32 +0700

    73#issuecomment-555828854

---
+++ Language/id/Encryption.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Encryption language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT	MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 4.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noDriverRequested'    => 'Tidak ada driver yang diminta; ',
+   'noHandlerAvailable'   => 'Tidak dapat menemukan penangan enkripsi {0} yang tersedia.',
+   'unKnownHandler'       => '"{0}" tidak dapat dikonfigurasi.',
+   'starterKeyNeeded'     => 'Encrypter membutuhkan kunci starter.',
+   'authenticationFailed' => 'Dekripsi: otentikasi gagal.',
+   'encryptionFailed'     => 'Enkripsi gagal.',
+];
---
+++ Language/id/Entity.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * CLI language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return
+[
+   'tryingToAccessNonExistentProperty' => 'Mencoba mengakses properti yang tidak ada {0} dari {1}',
+];
