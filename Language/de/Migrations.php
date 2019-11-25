c9d9f41f93f47cc9fd52d324566a1c2a4475d017
muh-ramadhan <muh.ramadhan@email.com>
2019-11-26 03:57:13 +0700

    Fix de Missing Keys (not yet translated)

--- Language/de/Migrations.php
+++ Language/de/Migrations.php
@@ -19,6 +19,7 @@ return [
    'missingTable'      => 'Migrations Tabelle muss gesetzt sein.',
    'invalidType'       => 'Es wurde eine ungültige Migrationsnummerierung angegeben: {0}',
    'disabled'          => 'Migrationen wurden geladen, sind aber deaktiviert oder falsch eingerichtet.',
+   'batchNotFound'     => 'Target batch not found: ',
    'notFound'          => 'Migrationsdatei nicht gefunden: ',
    'empty'             => 'Keine Migrationsdateien gefunden',
    'gap'               => 'Es gibt eine Lücke in der Migrationsfolge in der Nähe der Versionsnummer: ',
@@ -38,6 +39,8 @@ return [
    'writeError'        => 'Fehler beim Erstellen der Datei.',
    'migNumberError'    => 'Die Migrationsnummer muss dreistellig sein, und es dürfen keine Lücken in der Sequenz sein.',
 
+   'latest'            => 'Running all new migrations...',
+   'generalFault'      => 'Migration failed!',
    'toLatest'          => 'Migration auf die neueste Version...',
    'migInvalidVersion' => 'Ungültige Versionsnummer angegeben.',
    'toVersionPH'       => 'Migration auf Version %s...',
