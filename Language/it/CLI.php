2ba44960f706a5a9e5e97532ab7d9c97c65799e8
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-15 23:52:00 +0100

    2020

--- Language/it/CLI.php
+++ Language/it/CLI.php
@@ -2,20 +2,21 @@
 
 /**
  * CLI language strings.
-*
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
  *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
- * 
+ *
  * @codeCoverageIgnore
  */
+
 return [
-	'commandNotFound' => 'Command "{0}" not found.',
-	'helpUsage'       => 'Uso:',
+	'commandNotFound' => 'Comando "{0}" non trovato.',
+	'helpUsage'       => 'Utilizzo:',
 	'helpDescription' => 'Descrizione:',
 	'helpOptions'     => 'Opzioni:',
 	'helpArguments'   => 'Argomenti:',
