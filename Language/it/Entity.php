6050398e9c7a28f1e0594ac99fd29ab11a940cb0
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-25 22:29:12 +0100

    2020

--- Language/it/Entity.php
+++ Language/it/Entity.php
@@ -5,7 +5,7 @@
  *
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
- * @copyright  2019 CodeIgniter Foundation
+ * @copyright  2019-2020 CodeIgniter Foundation
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
  * @filesource
@@ -15,5 +15,5 @@
 
 return
 [
-   'tryingToAccessNonExistentProperty' => 'Trying to access non existent property {0} of {1}',
+   'tryingToAccessNonExistentProperty' => 'Tentativo di accedere alla proprietà inesistente {0} di {1}',
 ];
