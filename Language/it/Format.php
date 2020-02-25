42f40162f23a04dc03a827864e773115063c4916
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-25 22:32:52 +0100

    2020

--- Language/it/Format.php
+++ Language/it/Format.php
@@ -3,17 +3,18 @@
 /**
  * Format language strings.
  *
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
- *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
- * 
+ *
  * @codeCoverageIgnore
  */
+
 return [
-	'invalidJSON'      => 'Fallita l\'elaborazione della stringa json, errore: "{0}".',
-	'missingExtension' => 'L\'estensione SimpleXML è richiesta per il formato XML.',
+   'invalidJSON'      => 'Impossibile fare il parsing della stringa JSON, errore: "{0}".',
+   'missingExtension' => 'L\'estensione SimpleXML è richiesta per il formato XML.',
 ];
