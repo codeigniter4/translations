a6d5ed04a0f8a64b4b4a6213bbfa07a4da1e2c30
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-28 20:32:14 +0100

    2020

--- Language/it/View.php
+++ Language/it/View.php
@@ -2,21 +2,22 @@
 /**
  * View language strings.
  *
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
- *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
  */
+
 return [
-	'invalidCellMethod'     => '{class}::{method} non è un metodo valido.',
-	'missingCellParameters' => '{class}::{method} non ha alcun parametro.',
-	'invalidCellParameter'  => '{0} non è un nome valido del parametro.',
-	'noCellClass'           => 'Nessuna classe view cell fornita.',
-	'invalidCellClass'      => 'Impossibile trovare la classe view cell: {0}.',
-	'tagSyntaxError'        => 'Hai un errore di sintassi nei tuoi tags Parser: {0}',
+   'invalidCellMethod'     => '{class}::{method} non è un metodo valido.',
+   'missingCellParameters' => '{class}::{method} non ha alcun parametro.',
+   'invalidCellParameter'  => '{0} non è un nome valido per il parametro.',
+   'noCellClass'           => 'Nessuna classe view cell fornita.',
+   'invalidCellClass'      => 'Impossibile trovare la classe view cell: {0}.',
+   'tagSyntaxError'        => 'Hai un errore di sintassi nei tuoi parser tag: {0}',
 ];
