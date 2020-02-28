a590ce6710b08cd87db0926ec03846b14ba1c041
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-28 19:49:40 +0100

    2020

--- Language/it/Migrations.php
+++ Language/it/Migrations.php
@@ -1,57 +1,56 @@
-<?php
+<<?php
 
 /**
  * Migration language strings.
  *
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
- *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
  */
+
 return [
 	// Migration Runner
-	'missingTable'    => 'La tavola delle Migrazioni deve essere indicata.',
-//	'invalidType'     => 'E\' stata specificata una numerazione di migrazione non valida: {0}',
-	'disabled'        => 'Le Migrazioni sono state caricate ma sono disabilitate o configurate in modo non corretto.',
-	'notFound'        => 'File di Migrazione non trovato: ',
-	'batchNotFound'     => 'Target batch not found: ',
-	'empty'           => 'Nessun file di Migrazione è stato trovato',
-	'gap'             => 'Vi è una lacuna nella sequenza di migrazione vicino al numero di versione: ',
-	'classNotFound'   => 'Non è possibile trovare la classe di migrazione "%s" .',
-	'missingMethod'   => 'Alla classe di migrazione manca un metodo "%s" .',
+   'missingTable'      => 'La tabella delle migrazioni deve essere impostata.',
+   'disabled'          => 'Le migrazioni sono state caricate ma sono disabilitate o configurate in modo incorretto.',
+   'notFound'          => 'File di migrazione non trovato: ',
+   'batchNotFound'     => 'Target del batch non trovato: ',
+   'empty'             => 'Nessun file di migrazione trovato',
+   'gap'               => 'È presente un gap nella sequenza di migrazione vicino al numero di versione: ',
+   'classNotFound'     => 'Non è possibile trovare la classe di migrazione "%s".',
+   'missingMethod'     => 'Alla classe di migrazione manca un metodo "%s".',
 
 	// Migration Command
-	'migHelpLatest'   => "\t\tEffettua la migrazione del database alla ultima migrazione disponibile.",
-	'migHelpCurrent'  => "\t\tEffettua la migrazione del database alla versione selezionata come 'current' nella configurazione.",
-	'migHelpVersion'  => "\tEffettua la migrazione del database alla versione {v}.",
-	'migHelpRollback' => "\tElabora tutte le migrazioni 'down' alla versione 0.",
-	'migHelpRefresh'  => "\t\tDisinstalla e rielabora tutte le migrazioni a un database riavviato.",
-	'migHelpSeed'     => "\tAvvia il seminatore denominato [name].",
-	'migCreate'       => "\tCrea una nuova migrazione denominata [name]",
-	'nameMigration'   => 'Denomina il file di migrazione',
-	'badCreateName'   => 'Devi fornire un nome al file di migrazione.',
-	'writeError'      => 'Si è verificato un errore tentando di creare il file.',
-	'migNumberError'    => 'Migration number must be three digits, and there must not be any gaps in the sequence.',
+   'migHelpLatest'     => "\t\tElabora la migrazione del database all'ultima migrazione disponibile.",
+   'migHelpCurrent'    => "\t\tElabora la migrazione del database alla versione impostata come 'current' nella configurazione.",
+   'migHelpVersion'    => "\tElabora la migrazione del database alla versione {v}.",
+   'migHelpRollback'   => "\tElabora tutte le migrazioni 'down' fino alla versione 0.",
+   'migHelpRefresh'    => "\t\tDisinstalla e rielabora tutte le migrazioni verso un database riavviato.",
+   'migHelpSeed'       => "\tAvvia il seeder denominato [name].",
+   'migCreate'         => "\tCrea una nuova migrazione denominata [name]",
+   'nameMigration'     => 'Dai un nome al file di migrazione',
+   'badCreateName'     => 'Devi fornire un nome per il file di migrazione.',
+   'writeError'        => 'Errore nel tentativo di creazione del file.',
+   'migNumberError'    => 'Il numero di migrazione deve contenere tre cifre e non devono esserci spazi vuoti nella sequenza.',
 
-	'latest'            => 'Running all new migrations...',
-	'generalFault'      => 'Migration failed!', 
-//	'toLatest'          => 'Migrazione verso la più recente versione...',
-	'migInvalidVersion' => 'Fornito un numero di versione non valido.',
-	'toVersionPH'       => 'Migrazione verso la versione %s...',
-	'toVersion'         => 'Migrazione verso la versione corrente...',
-	'rollingBack'       => 'Ritorna indietro tutte le migrazioni...',
-	'noneFound'         => 'Nessuna migrazione è stata trovata.',
-	'on'                => 'Migrato a: ',
-	'migSeeder'         => 'Nome seminatrice',
-	'migMissingSeeder'  => 'Devi fornire un nome seminatrice.',
-	'removed'           => 'Tornando indietro: ',
-	'added'             => 'Elaborazione: ',
+   'latest'            => 'Esecuzione di tutte le nuove migrazioni...',
+   'generalFault'      => 'Migrazione fallita!',
+   'migInvalidVersion' => 'Fornito un numero di versione non valido.',
+   'toVersionPH'       => 'Migrazione alla versione %s...',
+   'toVersion'         => 'Migrazione alla versione corrente...',
+   'rollingBack'       => 'Rollback di tutte le migrazioni: ',
+   'noneFound'         => 'Nessuna migrazione trovata.',
+   'on'                => 'Migrato a: ',
+   'migSeeder'         => 'Nome seeder',
+   'migMissingSeeder'  => 'Devi fornire un nome per il seeder.',
+   'removed'           => 'Rollback: ',
+   'added'             => 'Elaborazione: ',
 
-	'version'           => 'Versione',
-	'filename'          => 'Nome file',
+   'version'           => 'Versione',
+   'filename'          => 'Nome file',
 ];
