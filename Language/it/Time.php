c617bc2d789f092b5df3bb051eabd746c624a164
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-28 20:10:57 +0100

    2020

--- Language/it/Time.php
+++ Language/it/Time.php
@@ -3,33 +3,34 @@
 /**
  * Time language strings.
  *
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
- *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
  */
+
 return [
-	'invalidMonth'   => 'Mesi deve essere fra 1 e 12. Indicato: {0}',
-    'invalidDay'     => 'Giorni deve essere fra 1 e 31. Indicato: {0}',
-    'invalidOverDay' => 'Giorni deve essere fra 1 e {0}. Indicato: {1}',
-	'invalidHours'   => 'Ore deve essere fra 0 e 23. Indicato: {0}',
-	'invalidMinutes' => 'Minuti deve essere fra 0 e 59. Indicato: {0}',
-	'invalidSeconds' => 'Secondi deve essere fra 0 e 59. Indicato: {0}',
-	'years'          => '{0, plural, =1{# year} other{# years}}',
-	'months'         => '{0, plural, =1{# month} other{# months}}',
-	'weeks'          => '{0, plural, =1{# week} other{# weeks}}',
-	'days'           => '{0, plural, =1{# day} other{# days}}',
-	'hours'          => '{0, plural, =1{# hour} other{# hours}}',
-	'minutes'        => '{0, plural, =1{# minute} other{# minutes}}',
-	'seconds'        => '{0, plural, =1{# second} other{# seconds}}',
-	'ago'            => '{0} fa',
-	'inFuture'       => 'fra {0}',
-	'yesterday'      => 'Ieri',
-	'tomorrow'       => 'Domani',
-	'now'            => 'Adesso',
+   'invalidMonth'   => 'I mesi devono essere tra 1 e 12. Indicato: {0}',
+   'invalidDay'     => 'I giorni devono essere tra 1 e 31. Indicato: {0}',
+   'invalidOverDay' => 'I giorni devono essere tra 1 e {0}. Indicato: {1}',
+   'invalidHours'   => 'Le ore devono essere tra 0 e 23. Indicato: {0}',
+   'invalidMinutes' => 'I minuti devono essere tra 0 e 59. Indicato: {0}',
+   'invalidSeconds' => 'I secondi devono essere tra 0 e 59. Indicato: {0}',
+   'years'          => '{0, plural, =1{# anno} other{# anni}}',
+   'months'         => '{0, plural, =1{# mese} other{# mesi}}',
+   'weeks'          => '{0, plural, =1{# settimana} other{# settimane}}',
+   'days'           => '{0, plural, =1{# giorno} other{# giorni}}',
+   'hours'          => '{0, plural, =1{# ora} other{# ore}}',
+   'minutes'        => '{0, plural, =1{# minuto} other{# minuti}}',
+   'seconds'        => '{0, plural, =1{# secondo} other{# secondi}}',
+   'ago'            => '{0} fa',
+   'inFuture'       => 'il {0}',
+   'yesterday'      => 'Ieri',
+   'tomorrow'       => 'Domani',
+   'now'            => 'Adesso',
 ];
