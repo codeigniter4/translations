662a88268c5b476dc34eaa6256435e9f5163d154
Gianluigi Vizzielli <GianVizzielli@users.noreply.github.com>
2020-02-28 19:53:46 +0100

    2020

--- Language/it/Number.php
+++ Language/it/Number.php
@@ -3,28 +3,28 @@
 /**
  * Number language strings.
  *
- * @package      CodeIgniter
- * @author       Luca Marco Rasia
- * @license      https://opensource.org/licenses/MIT	MIT License
- * @link         https://codeigniter.com
- * @since        Version 4.0.0
-
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
- * 
+ *
  * @codeCoverageIgnore
  */
 
 return [
-	'terabyteAbbr' => 'TB',
-	'gigabyteAbbr' => 'GB',
-	'megabyteAbbr' => 'MB',
-	'kilobyteAbbr' => 'KB',
-	'bytes'        => 'Bytes',
+   'terabyteAbbr' => 'TB',
+   'gigabyteAbbr' => 'GB',
+   'megabyteAbbr' => 'MB',
+   'kilobyteAbbr' => 'KB',
+   'bytes'        => 'Byte',
 
 	// don't forget the space in front of these!
-	'thousand'     => ' mille',
-	'million'      => ' milione',
-	'billion'      => ' miliardo',
-	'trillion'     => ' mille miliardi',
-	'quadrillion'  => ' milione di miliardi',
+   'thousand'     => ' mila',
+   'million'      => ' milione',
+   'billion'      => ' miliardo',
+   'trillion'     => ' trilione',
+   'quadrillion'  => ' quadrilione',
 ];
