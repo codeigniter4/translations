ad538f59b7f3ff67b905d25237334b23140151db
Johan Smolders <johan.smolders@gmail.com>
2020-03-07 22:50:07 +0100

    Initial NL translation

---
+++ Language/nl/Language.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Language system language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+    'languageGetLineInvalidArgumentException' => 'Get line moet een string of array van strings zijn.',
+];
+
