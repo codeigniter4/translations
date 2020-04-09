37f2737776a457f356e6366f9e5d18bd1b094190
Timothy Lee <toydiary@gmail.com>
2020-04-09 23:23:35 +0900

    Update: Korean language pack for CI v4.0.0 and v4.0.2

--- Language/ko/Entity.php
+++ Language/ko/Entity.php
@@ -5,7 +5,7 @@
  *
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
- * @copyright  2019 CodeIgniter Foundation
+ * @copyright  2019-2020 CodeIgniter Foundation
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
  * @filesource
@@ -15,5 +15,5 @@
 
 return
 [
-   'tryingToAccessNonExistentProperty' => '존재하지 않는 속성 {0}에 접근을 시도 중이며, 해당 속성은 {1}에 포함되어 있습니다.', // CI4 rc.3기준 : tryingToAccessNonExistentProperty 메시지가 사용된 곳이 없어서 차후 번역이 변경될 수 있음. // 'Trying to access non existent property {0} of {1}',
+   'tryingToAccessNonExistentProperty' => '{1}의 존재하지 않는 속성 {0}에 접근을 시도 중입니다.', // 'Trying to access non existent property {0} of {1}',
 ];
