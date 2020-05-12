661844e865e756740e6d0fda5d48e7eb168d0847
rakheshthayyur <rakheshthayyur@gmail.com>
2020-05-12 14:45:28 +0530

    Update Session.php

--- Language/ml/Session.php
+++ Language/ml/Session.php
@@ -15,9 +15,9 @@
  */
 
 return [
-   'missingDatabaseTable'   => '`sessionSavePath` must have the table name for the Database Session Handler to work.',
-   'invalidSavePath'        => 'Session: Configured save path "{0}" is not a directory, does not exist or cannot be created.',
-   'writeProtectedSavePath' => 'Session: Configured save path "{0}" is not writable by the PHP process.',
-   'emptySavePath'          => 'Session: No save path configured.',
-   'invalidSavePathFormat'  => 'Session: Invalid Redis save path format: {0}',
+   'missingDatabaseTable'   => '`sessionSavePath` ഡാറ്റാബേസ് സെഷൻ ഹാൻഡ്‌ലർ പ്രവർത്തിക്കുന്നതിന് പട്ടികയുടെ പേര് ഉണ്ടായിരിക്കണം.',
+   'invalidSavePath'        => 'Session: കോൺഫിഗർ ചെയ്ത സേവ് പാത്ത് "{0}" ഒരു ഡയറക്ടറിയല്ല, നിലവിലില്ല അല്ലെങ്കിൽ സൃഷ്ടിക്കാൻ കഴിയില്ല.',
+   'writeProtectedSavePath' => 'Session: കോൺഫിഗർ ചെയ്ത സേവ് പാത്ത് "{0}" പി‌എച്ച്പി പ്രോസസ്സ് എഴുതാൻ കഴിയില്ല.',
+   'emptySavePath'          => 'Session: സേവ് പാത്ത് ക്രമീകരിച്ചിട്ടില്ല.',
+   'invalidSavePathFormat'  => 'Session: അസാധുവായ റെഡിസ് പാത്ത് ഫോർമാറ്റ് സംരക്ഷിക്കുക: {0}',
 ];
