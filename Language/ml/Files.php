1132cf9125b804dbc3bf353fb5987a18dc49ab6b
rakheshthayyur <rakheshthayyur@gmail.com>
2020-05-12 13:53:46 +0530

    Update Files.php

--- Language/ml/Files.php
+++ Language/ml/Files.php
@@ -1,13 +1,13 @@
 <?php
+
 /**
- * Files language strings.
+ * CLI language strings.
  *
  * @package    CodeIgniter
- * @author     Rakhesh Thayyur
- * @copyright  2019-2020
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
- * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
