aa538f9921c4e11722b84d32a965f783fc918edc
rakheshthayyur <rakheshthayyur@gmail.com>
2020-05-12 13:47:08 +0530

    Create Files.php

---
+++ Language/ml/Files.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Files language strings.
+ *
+ * @package    CodeIgniter
+ * @author     Rakhesh Thayyur
+ * @copyright  2019-2020
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'fileNotFound' => 'ഫയൽ കാണുന്നില്ല: {0}',
+   'cannotMove'   => '{0} മുതൽ {1} വരെ ഫയൽ നീക്കാൻ കഴിഞ്ഞില്ല({2})',
+];
