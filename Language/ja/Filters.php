ecbeb21ea187aabc485ccf7ed3a6aecca2cf2fee
kohenji <kohenji@sarahsystems.com>
2020-05-13 18:59:16 +0900

    Japanese translations added.

---
+++ Language/ja/Filters.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Filters language strings.
+ *
+ * @package    CodeIgniter
+ * @author     Shoji Ogura
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'noFilter'           => '{0} フィルタには、一致するエイリアスが定義されていなければなりません。', //{0} filter must have a matching alias defined.
+   'incorrectInterface' => '{0} は CodeIgniter\Filters\FilterInterface を実装する必要があります。', //{0} must implement CodeIgniter\Filters\FilterInterface.
+];
