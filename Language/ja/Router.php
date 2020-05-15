94503a9685185ba0c3b3c96c7d35bfe94bccc295
kohenji <kohenji@sarahsystems.com>
2020-05-15 11:03:30 +0900

    Added Japanese translations.

---
+++ Language/ja/Router.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Router language strings.
+ *
+ * @package    CodeIgniter
+ * @author     Shoji Ogura
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidParameter'    => 'パラメータが予想される型と一致しません。', //A parameter does not match the expected type.
+   'missingDefaultRoute' => '何を表示するか決定できません。デフォルトルートがルーティングファイルで指定されていません。', //Unable to determine what should be displayed. A default route has not been specified in the routing file.
+];
