6ed21ef8776af51e17301c39c71542d4d2d4c65d
Natan Felles <natanfelles@gmail.com>
2020-10-23 15:57:19 -0300

    Add Fabricator and Seed

---
+++ Language/pt-BR/Fabricator.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Fabricator language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidModel'      => 'Model fornecido para fabricação é inválido.',
+   'missingFormatters' => 'Nenhum formatador válido definido.',
+];
---
+++ Language/pt-BR/Seed.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Seed language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+    // Seed command
+    'nameFile'   => 'Nomeie o arquivo seeder',
+    'writeError' => 'Erro ao tentar criar o  arquivo {0}, verifique se o diretório é gravável.',
+];
