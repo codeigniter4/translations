23f012e024a5c3d2f1b750026399e0a8074b521b
totoprayogo1916 <totoprayogo1916@gmail.com>
2020-11-11 10:41:12 +0700

    Use short notices for header docblock

--- Language/ar/Validation.php
+++ Language/ar/Validation.php
@@ -1,42 +1,15 @@
 <?php
 
 /**
- * Validation language strings.
+ * This file is part of the CodeIgniter 4 framework.
  *
- * This content is released under the MIT License (MIT)
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
- * Copyright (c) 2014-2019 British Columbia Institute of Technology
- * Copyright (c) 2019-2020 CodeIgniter Foundation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package    CodeIgniter
- * @author     CodeIgniter Dev Team
- * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT	MIT License
- * @link       https://codeigniter.com
- * @since      Version 4.0.0
- * @filesource
- *
- * @codeCoverageIgnore
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
+// Validation language settings
 return [
 	// Core Messages
 	'noRuleSets'            => 'لم يتم تحديد مجموعة قواعد التحقق في الاعدادات.',
@@ -61,7 +34,7 @@ return [
 	'integer'               => 'الحقل {field} يجب أن يحتوى على رقم صحيح.',
 	'is_natural'            => 'الحقل {field} يجب أن يحتوى على عدد.',
 	'is_natural_no_zero'    => 'الحقل {field} يجب أن يحتوى على عدد أكبر من الصفر.',
-   'is_not_unique'         => 'The {field} field must contain a previously existing value in the database.',
+	'is_not_unique'         => 'The {field} field must contain a previously existing value in the database.',
 	'is_unique'             => 'الحقل {field} يجب أن يحتوى على قيمة غير موجودة مسبقا.',
 	'less_than'             => 'الحقل {field} يجب أن يحتوى على رقم أقل من {param}.',
 	'less_than_equal_to'    => 'الحقل {field} يجب أن يحتوى على رقم أقل أو يساوي {param}.',
