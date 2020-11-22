0c096773d5b52253a06e814eed06ec6c537b4827
Dāvis Lasis <davis.lasis@gmail.com>
2020-11-22 21:42:37 +0200

    refactored to short doc blocks, added missing keys, commented out excess keys for now.

--- Language/lv/Database.php
+++ Language/lv/Database.php
@@ -1,48 +1,21 @@
 <?php
 
 /**
- * Database language strings.
+ * This file is part of the CodeIgniter 4 framework.
  *
- * This content is released under the MIT License (MIT)
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
- * Copyright (c) 2014-2019 British Columbia Institute of Technology
- * Copyright (c) 2019-2020 CodeIgniter Foundation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package    CodeIgniter
- * @author     Dāvis Lasis <davis.lasis@gmail.com>
- * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT MIT License
- * @link       https://codeigniter.com
- * @since      Version 4.0.0
- * @filesource
- *
- * @codeCoverageIgnore
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
+// Database language settings
 return [
    'invalidEvent'                     => 'Modeļa notikuma atgriezeniskās saiknes funkcija {0} nav derīga.',
    'invalidArgument'                  => 'Jums jānorāda derīgs arguments {0}.',
    'invalidAllowedFields'             => 'Modelim ir jānorāda atļautie lauki: {0}',
    'emptyDataset'                     => 'Nav datu par {0}.',
-   'emptyPrimaryKey'                  => 'Nav definēta galvenā atslēga mēģinot izveidot {0}.',
+   //'emptyPrimaryKey'                  => 'Nav definēta galvenā atslēga mēģinot izveidot {0}.',
    'failGetFieldData'                 => 'Neizdevās iegūt lauka datus no datu bāzes.',
    'failGetIndexData'                 => 'Neizdevās iegūt indeksa datus no datu bāzes.',
    'failGetForeignKeyData'            => 'Neizdevās iegūt ārējās atslēgas datus no datu bāzes.',
