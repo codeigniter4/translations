89b2aeca4dc00abe3478f5e3607acecfbb1813b6
Terrorboy <z9n@kakao.com>
2020-11-30 00:29:18 +0900

    Update Language/ko/Session.php
    Co-authored-by: John Paul E. Balandan, CPA <51850998+paulbalandan@users.noreply.github.com>

--- Language/ko/Session.php
+++ Language/ko/Session.php
@@ -16,5 +16,5 @@ return [
    'writeProtectedSavePath' => 'Session: 설정된 세션 저장 경로 "{0}"의 경우, PHP 프로세스에 의해 쓰기를 할 수 없습니다.', // 'Session: Configured save path "{0}" is not writable by the PHP process.',
    'emptySavePath'          => 'Session: 저장 경로가 설정되어 있지 않습니다.', // 'Session: No save path configured.',
    'invalidSavePathFormat'  => 'Session: 유효하지 않는 Redis 세션 저장 경로 형식: {0}', // 'Session: Invalid Redis save path format: {0}',
-   'invalidSameSiteSetting' => '세션 : SameSite 설정은 None, Lax, Strict 또는 빈 문자열이어야합니다. 주어진 값 : {0}', // 'Session: The SameSite setting must be None, Lax, Strict, or a blank string. Given: {0}',
+   'invalidSameSiteSetting' => 'Session : SameSite 설정은 None, Lax, Strict 또는 빈 문자열이어야합니다. 주어진 값 : {0}', // 'Session: The SameSite setting must be None, Lax, Strict, or a blank string. Given: {0}',
 ];
