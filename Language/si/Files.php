84bcdd99d3cfcfeb36e880d94ef60b86e5fb9e8e
Nalaka Prasad <info@nalakaprasad.com>
2020-12-19 13:37:18 +0530

    Files.php lang file all the si translations.

--- Language/si/Files.php
+++ Language/si/Files.php
@@ -11,6 +11,6 @@
 
 // Files language settings
 return [
-	'fileNotFound' => 'File not found: {0}',
-	'cannotMove'   => 'Could not move file {0} to {1} ({2})',
+	'fileNotFound' => 'ගොනුව හමු නොවිණි: {0}',
+	'cannotMove'   => 'ගොනුව ගෙනයාමට නොහැකි විය {0} සිට {1} ({2})',
 ];
