2135e848b6671dc945403e9b5c33ec7300d91c7b
Nalaka Prasad <info@nalakaprasad.com>
2020-12-19 12:04:14 +0530

    Router.php lang file all the si translations.

--- Language/si/Router.php
+++ Language/si/Router.php
@@ -11,6 +11,6 @@
 
 // Router language settings
 return [
-	'invalidParameter'    => 'A parameter does not match the expected type.',
-	'missingDefaultRoute' => 'Unable to determine what should be displayed. A default route has not been specified in the routing file.',
+	'invalidParameter'    => 'පැරාමීටරයක් අපේක්ෂිත වර්ගය සමග ගැලපෙන්නේ නැත.',
+	'missingDefaultRoute' => 'පෙන්විය යුතු දේ තීරණය කිරීමට නොහැක. ඩිෆෝල්ට් රවුට් එකක් රවුටින් ගොනුවේ සඳහන් කර නොමැත.',
 ];
