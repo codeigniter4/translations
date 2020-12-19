ece3b4aa63ee0145661214b8a1a568ec5d3604a9
Nalaka Prasad <info@nalakaprasad.com>
2020-12-19 10:39:53 +0530

    Add all ci4-develop lang files into si directory.

---
+++ Language/si/CLI.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// CLI language settings
+return [
+	'commandNotFound'     => 'Command "{0}" not found.',
+	'altCommandSingular'  => 'Did you mean this?',
+	'altCommandPlural'    => 'Did you mean one of these?',
+	'helpUsage'           => 'Usage:',
+	'helpDescription'     => 'Description:',
+	'helpOptions'         => 'Options:',
+	'helpArguments'       => 'Arguments:',
+	'invalidColor'        => 'Invalid {0} color: {1}.',
+
+	// Generators
+	'generateClassName'   => 'Name of class',
+	'generateParentClass' => 'Name of parent class to extend from',
+	'generateFileExists'  => '{0} already exists.',
+	'generateFileSuccess' => 'Created file: ',
+	'generateFileError'   => 'Error in creating file: ',
+	'namespaceNotDefined' => 'Namespace "{0}" is not defined.',
+];
