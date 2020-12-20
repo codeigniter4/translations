da03425c6877d5baf327cd2cdf052c073e0dc2db
Nalaka Prasad <info@nalakaprasad.com>
2020-12-20 22:35:16 +0530

    Format.php lang file all the si translations.

--- Language/si/Format.php
+++ Language/si/Format.php
@@ -11,8 +11,8 @@
 
 // Format language settings
 return [
-	'invalidFormatter' => '"{0}" is not a valid Formatter class.', // "{0}" is not a valid Formatter class.
-	'invalidJSON'      => 'Failed to parse json string, error: "{0}".', // Failed to parse json string, error: "{0}".
-	'invalidMime'      => 'No Formatter defined for mime type: "{0}".', // No Formatter defined for mime type: "{0}".
-	'missingExtension' => 'The SimpleXML extension is required to format XML.', // The SimpleXML extension is required to format XML.
+	'invalidFormatter' => '"{0}" යනු වලංගු Formatter class එකක් නොවේ.', // "{0}" is not a valid Formatter class.
+	'invalidJSON'      => 'අසමත් වුනා json string එක parse කිරීමට, දෝෂය: "{0}".', // Failed to parse json string, error: "{0}".
+	'invalidMime'      => 'mime type: "{0}" සඳහා කිසිඳු Formatter එකක් අර්ථ දක්වා නොමැත.', // No Formatter defined for mime type: "{0}".
+	'missingExtension' => 'XML ෆෝර්මැට් කිරීම සඳහා SimpleXML එක්ස්ටෙන්ෂන් එක අවශ්‍ය වේ.', // The SimpleXML extension is required to format XML.
 ];
