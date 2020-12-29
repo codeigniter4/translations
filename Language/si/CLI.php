a01d60c6fe78736a387d6cc92fe7bcfc5361e201
Nalaka Prasad <info@nalakaprasad.com>
2020-12-29 23:09:27 +0530

    CLI.php lang file all the translations.

--- Language/si/CLI.php
+++ Language/si/CLI.php
@@ -11,20 +11,20 @@
 
 // CLI language settings
 return [
-	'commandNotFound'     => 'Command "{0}" not found.', // Command "{0}" not found.
-	'altCommandSingular'  => 'Did you mean this?', // Did you mean this?
-	'altCommandPlural'    => 'Did you mean one of these?', // Did you mean one of these?
-	'helpUsage'           => 'Usage:', // Usage:
-	'helpDescription'     => 'Description:', // Description:
-	'helpOptions'         => 'Options:', // Options:
-	'helpArguments'       => 'Arguments:', // Arguments:
-	'invalidColor'        => 'Invalid {0} color: {1}.', // Invalid {0} color: {1}.
+	'commandNotFound'     => '"{0}" විධානය හමු නොවීය.', // Command "{0}" not found.
+	'altCommandSingular'  => 'ඔබ මෙය අදහස් කළේද?', // Did you mean this?
+	'altCommandPlural'    => 'ඔබ අදහස් කළේ මෙයින් එකක්ද?', // Did you mean one of these?
+	'helpUsage'           => 'භාවිතය:', // Usage:
+	'helpDescription'     => 'විස්තරය:', // Description:
+	'helpOptions'         => 'විකල්ප:', // Options:
+	'helpArguments'       => 'තර්ක:', // Arguments:
+	'invalidColor'        => 'අවලංගු {0} වර්ණය: {1}.', // Invalid {0} color: {1}.
 
 	// Generators
-	'generateClassName'   => 'Name of class', // Name of class
-	'generateParentClass' => 'Name of parent class to extend from', // Name of parent class to extend from
-	'generateFileExists'  => '{0} already exists.', // {0} already exists.
-	'generateFileSuccess' => 'Created file: ', // Created file: 
-	'generateFileError'   => 'Error in creating file: ', // Error in creating file: 
-	'namespaceNotDefined' => 'Namespace "{0}" is not defined.', // Namespace "{0}" is not defined.
+	'generateClassName'   => 'Class එකේ නම', // Name of class
+	'generateParentClass' => 'Extend කිරීම සඳහා parent class හි නම', // Name of parent class to extend from
+	'generateFileExists'  => '{0} දැනටමත් පවතී.', // {0} already exists.
+	'generateFileSuccess' => 'සාදන ලද ගොනුව: ', // Created file: 
+	'generateFileError'   => 'ගොනුව සෑදීමේ ඇතිවූ දෝෂයකි: ', // Error in creating file: 
+	'namespaceNotDefined' => 'නේම්ස්පේස් "{0}" අර්ථ දක්වා නැත.', // Namespace "{0}" is not defined.
 ];
