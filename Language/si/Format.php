fb19065d4b49b945dd0fa8adde769a38c7f442d4
Nalaka Prasad <info@nalakaprasad.com>
2020-12-29 21:09:40 +0530

    Simple letter case change.

--- Language/si/Format.php
+++ Language/si/Format.php
@@ -13,6 +13,6 @@
 return [
 	'invalidFormatter' => '"{0}" යනු වලංගු Formatter class එකක් නොවේ.', // "{0}" is not a valid Formatter class.
 	'invalidJSON'      => 'අසමත් වුනා json string එක parse කිරීමට, දෝෂය: "{0}".', // Failed to parse json string, error: "{0}".
-	'invalidMime'      => 'mime type: "{0}" සඳහා කිසිඳු Formatter එකක් අර්ථ දක්වා නොමැත.', // No Formatter defined for mime type: "{0}".
+	'invalidMime'      => 'MIME type: "{0}" සඳහා කිසිඳු Formatter එකක් අර්ථ දක්වා නොමැත.', // No Formatter defined for mime type: "{0}".
 	'missingExtension' => 'XML ෆෝර්මැට් කිරීම සඳහා SimpleXML එක්ස්ටෙන්ෂන් එක අවශ්‍ය වේ.', // The SimpleXML extension is required to format XML.
 ];
