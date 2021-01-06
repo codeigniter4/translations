f6dfc670017d6b1a0c640f9d98361bd8dbcfa72f
Matus Sopko <47972956+matussopko@users.noreply.github.com>
2021-01-06 02:04:15 +0100

    update

---
+++ Language/sk/Fabricator.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Fabricator language settings
+return [
+	'invalidModel'      => 'Na výrobu bol dodaný neplatný model.',
+	'missingFormatters' => 'Nie sú definované žiadne platné formátory.',
+	'createFailed'      => 'Výrobcu sa nepodarilo vložiť do tabuľky {0}: {1}.',
+];
