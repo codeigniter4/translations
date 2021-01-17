ec998013261c63662cbb06463e8e937541c38b7e
kenjis <kenji.uui@gmail.com>
2021-01-17 17:22:41 +0900

    Delete unnecessary spaces

--- Language/ja/Time.php
+++ Language/ja/Time.php
@@ -12,12 +12,12 @@
 // Time language settings
 return [
 	'invalidFormat'  => '"{0}" は無効な日時です', //"{0}" is not a valid datetime format
-	'invalidMonth'   => '月は1から12を指定してください。 入力された値: {0}', //Months must be between 1 and 12. Given: {0}
-	'invalidDay'     => '日は1から31を指定してください。 入力された値: {0}', //Days must be between 1 and 31. Given: {0}
-	'invalidOverDay' => '日は1から{0}を指定してください。 入力された値: {1}', //Days must be between 1 and {0}. Given: {1}
-	'invalidHours'   => '時は0から23を指定してください。 入力された値: {0}', //Hours must be between 0 and 23. Given: {0}
-	'invalidMinutes' => '分は0から59を指定してください。 入力された値: {0}', //Minutes must be between 0 and 59. Given: {0}
-	'invalidSeconds' => '秒は0から59を指定してください。 入力された値: {0}', //Seconds must be between 0 and 59. Given: {0}
+	'invalidMonth'   => '月は1から12を指定してください。入力された値: {0}', //Months must be between 1 and 12. Given: {0}
+	'invalidDay'     => '日は1から31を指定してください。入力された値: {0}', //Days must be between 1 and 31. Given: {0}
+	'invalidOverDay' => '日は1から{0}を指定してください。入力された値: {1}', //Days must be between 1 and {0}. Given: {1}
+	'invalidHours'   => '時は0から23を指定してください。入力された値: {0}', //Hours must be between 0 and 23. Given: {0}
+	'invalidMinutes' => '分は0から59を指定してください。入力された値: {0}', //Minutes must be between 0 and 59. Given: {0}
+	'invalidSeconds' => '秒は0から59を指定してください。入力された値: {0}', //Seconds must be between 0 and 59. Given: {0}
 	'years'          => '{0, plural, =1{# 年} other{# 年}}', //{0, plural, =1{# year} other{# years}}
 	'months'         => '{0, plural, =1{# ヶ月} other{# ヶ月}}', //{0, plural, =1{# month} other{# months}}
 	'weeks'          => '{0, plural, =1{# 週間} other{# 週間}}', //{0, plural, =1{# week} other{# weeks}}
