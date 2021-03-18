c3cc3b58460b2d846153faca671bc9acc3c73e64
Terrorboy <z9n@kakao.com>
2021-03-18 23:52:00 +0900

    ko update

--- Language/ko/Cast.php
+++ Language/ko/Cast.php
@@ -17,4 +17,7 @@ return [
 	'jsonErrorSyntax'        => 'JSON 구문 오류', // Syntax error, malformed JSON
 	'jsonErrorUtf8'          => 'UTF-8 문자열 구문 오류. 인코딩이 잘못되었을 수도 있습니다.', // Malformed UTF-8 characters, possibly incorrectly encoded
 	'jsonErrorUnknown'       => '알 수 없는 에러', // Unknown error
+	'abstractCastMissing'    => '{0} 클래스는 CodeIgniter\EntityCast\AbstractCast 클래스를 상속해야합니다.', // The {0} class must inherit the CodeIgniter\EntityCast\AbstractCast class.
+	'wrongCastMethod'        => '캐스트에 허용되는 메소드는 "get"및 "set"입니다.', // Allowed methods for cast is "get" and "set".
+	'invalidTimestamp'       => '"timestamp" 캐스팅 유형은 timestamp여야 합니다.', // Type casting "timestamp" expects a correct timestamp.
 ];
