566c0333729ecb0ac342d96e328641adb6a93fa7
Stefan Bauer <sba@gmx.ch>
2021-07-10 14:50:23 +0200

    [german] add missing string to Core.php

--- Language/de/Core.php
+++ Language/de/Core.php
@@ -14,6 +14,7 @@ return [
 	'copyError'                    => 'Beim Versuch, die Datei ({0}) zu ersetzen, ist ein Fehler aufgetreten. Bitte sicherstellen, dass das Verzeichnis beschreibbar ist.',
 	'enabledZlibOutputCompression' => 'Die zlib.output_compression ini Direktive ist aktiviert. Dies funktioniert nicht gut mit Ausgabe-Pufferung.',
 	'invalidFile'                  => 'Ungültige Datei: {0}',
+    'invalidPhpVersion'            => 'Um CodeIgniter ausführen zu können, muss PHP in der Version {0} oder höher installiert sein. Aktuelle Version: {1}',
 	'missingExtension'             => 'Für das Framework müssen die folgende(n) Erweiterung(en) installiert und geladen werden: {0}.',
 	'noHandlers'                   => '{0} muss mindestens einen Handler bereitstellen.',
 ];
