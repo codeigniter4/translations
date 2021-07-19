b22da7fb332c2fc4e009fdd0e42247697eb3a7a9
grisinformatica <42251975+grisinformatica@users.noreply.github.com>
2021-07-19 10:46:02 -0300

    Update Cast.php

--- Language/es/Cast.php
+++ Language/es/Cast.php
@@ -11,10 +11,13 @@
 
 // Cast language settings
 return [
+	'baseCastMissing'        => 'La clase "{0}" debe heredar la clase "CodeIgniter\Entity\Cast\BaseCast".', //' The "{0}" class must inherit the "CodeIgniter\Entity\Cast\BaseCast" class.'
+	'invalidCastMethod'      => 'El método de transmisión "{0}" es inválido, los métodos válidos son: ["get", "set"].',
+	'invalidTimestamp'       => 'El tipo de transmisión "timestamp" espera un timestamp correcto.', // 'Type casting "timestamp" expects a correct timestamp.'
+	'jsonErrorCtrlChar'      => 'Se ha encontrado un carácter de control inesperado', // 'Unexpected control character found.',
 	'jsonErrorDepth'         => 'Se ha excedido el nivel máximo de profundidad', // 'Maximum stack depth exceeded',
 	'jsonErrorStateMismatch' => 'Desbordamiento o desajuste de modo', // 'Underflow or the modes mismatch',
-	'jsonErrorCtrlChar'      => 'Se ha encontrado un carácter de control inesperado', // 'Unexpected control character found',
 	'jsonErrorSyntax'        => 'Error de sintaxis, JSON mal formado', // 'Syntax error, malformed JSON',
-	'jsonErrorUtf8'          => 'Carácter UTF-8 mal formado, posible codificación incorrecta', // 'Malformed UTF-8 characters, possibly incorrectly encoded',
 	'jsonErrorUnknown'       => 'Error desconocido', // 'Unknown error',
+	'jsonErrorUtf8'          => 'Carácter UTF-8 mal formado, posible codificación incorrecta', // 'Malformed UTF-8 characters, possibly incorrectly encoded',
 ];
