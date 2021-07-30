5a18470325992158302d2888210de70085d67d9f
Eyad Bereh <eyad@ixcoders.com>
2021-07-31 02:45:29 +0300

    Fixed spelling error for missingDatabaseTable and added missing translation for invalidSameSiteSetting

--- Language/ar/Session.php
+++ Language/ar/Session.php
@@ -11,9 +11,12 @@
 
 // Session language settings
 return [
-	'missingDatabaseTable'   => '`sessionSavePath` يجب أن يحتوى على إسم الجدول حتى يعمل النظام بشكل صحيح.',
+	'missingDatabaseTable'   => '`sessionSavePath` يجب أن يحتوي على إسم الجدول حتى يعمل النظام بشكل صحيح.',
 	'invalidSavePath'        => 'نظام الجلسة: المسار المحدد "{0}" ليس مجلد, أو غير موجود ولا يمكن انشاؤه.',
 	'writeProtectedSavePath' => 'نظام الجلسة: المسار المحدد "{0}" غير قابل للكتابة.',
 	'emptySavePath'          => 'نظام الجلسة: لا يوجد مسار للحفظ.',
 	'invalidSavePathFormat'  => 'نظام الجلسة: مسار خادم  Redis غير صحيح: {0}',
+
+	// @deprecated
+	'invalidSameSiteSetting' => 'يجب أن يحتوي إعداد SameSite على إحدى القيم التالية: ["None", "Lax", "Strict"] أو نص فارغ. القيمة التي تم وضعها هي {0}.',
 ];
