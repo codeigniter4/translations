fea0601fa7b309459fcdd49d9bf2264087a8532f
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-09 00:59:46 +0700

    translated the Casted.php

---
+++ Language/th/Cast.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cast language settings
+return [
+    'baseCastMissing'        => 'คลาส "{0}" จะต้องสืบทอดคุณสมบัติจากคลาส "CodeIgniter\Entity\Cast\BaseCast"',
+    'invalidCastMethod'      => 'แคสต์เมธอด "{0}" ไม่ถูกต้อง เมธอดที่ถูกต้องคือ: ["get", "set"]',
+    'invalidTimestamp'       => 'การแคสต์ข้อมูลประเภท "timestamp" จะต้องใช้ค่า timestamp ที่ถูกต้อง',
+    'jsonErrorCtrlChar'      => 'พบอักขระควบคุมที่ไม่ถูกต้อง',
+    'jsonErrorDepth'         => 'เกินความลึกสูงสุดของ stack',
+    'jsonErrorStateMismatch' => 'จำนวนโหมดน้อยเกินไปหรือโหมดไม่ตรงกัน',
+    'jsonErrorSyntax'        => 'เกิดข้อผิดพลาดทางไวยากรณ์ JSON มีรูปแบบไม่ถูกต้อง',
+    'jsonErrorUnknown'       => 'เกิดข้อผิดพลาดที่ไม่รู้จัก',
+    'jsonErrorUtf8'          => 'อักขระ UTF-8 ที่มีรูปแบบไม่ถูกต้อง อาจเกิดจากการเข้ารหัสที่ไม่ถูกต้อง',
+];
