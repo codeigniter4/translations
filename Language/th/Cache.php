7a63ce5371bf3388f8c8c12f4713992199983cb8
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-09 00:44:25 +0700

    translated the messages of Cache to Thai

---
+++ Language/th/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cache language settings
+return [
+    'unableToWrite'   => 'ไม่สามารถเขียนแคชไปยัง {0}',
+    'invalidHandlers' => 'การกำหนดค่าแคชจะต้องมีอาร์เรย์ของ $validHandlers อยู่ด้วย',
+    'noBackup'        => 'การกำหนดค่าแคชจะต้อมีการกำหนด handler และ backupHandler',
+    'handlerNotFound' => 'การกำหนดค่าแคชจะต้องมีตัวรับ (handler) ที่ถูกต้องหรือมีการระบุตัวรับสำรอง(backup handler)',
+];
