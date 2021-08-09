758075d10c275414551af5929e99453977eb4fa2
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-09 20:37:21 +0700

    Translated to Thai

---
+++ Language/th/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Database language settings
+return [
+    'invalidEvent'                     => '{0} ไม่ใช่การเรียกกลับของอีเวนต์โมเดลที่ถูกต้อง',
+    'invalidArgument'                  => 'คุณต้องระบุ {0} ที่ถูกต้อง',
+    'invalidAllowedFields'             => 'ต้องระบุฟิลด์ที่อนุญาตสำหรับโมเดล: {0}',
+    'emptyDataset'                     => 'ไม่มีข้อมูลสำหรับ {0}',
+    'emptyPrimaryKey'                  => 'ไม่มีคีย์หลักที่กำหนดไว้เมื่อพยายามสร้าง {0}',
+    'failGetFieldData'                 => 'ไม่สามารถรับข้อมูลฟิลด์จากฐานข้อมูล',
+    'failGetIndexData'                 => 'ไม่สามารถรับข้อมูลดัชนี (index) จากฐานข้อมูล',
+    'failGetForeignKeyData'            => 'ไม่สามารถรับข้อมูลคีย์นอก (foreign key) จากฐานข้อมูล',
+    'parseStringFail'                  => 'การแยกสตริงคีย์ล้มเหลว',
+    'featureUnavailable'               => 'คุณสมบัตินี้ไม่พร้อมใช้งานสำหรับฐานข้อมูลที่คุณกำลังใช้',
+    'tableNotFound'                    => 'ไม่พบตาราง `{0}` ในฐานข้อมูลปัจจุบัน',
+    'noPrimaryKey'                     => 'คลาสโมเดล `{0}` ไม่ได้ระบุคีย์หลัก',
+    'noDateFormat'                     => 'คลาสโมเดล `{0}` ไม่มี dateFormat ที่ถูกต้อง',
+    'fieldNotExists'                   => 'ไม่พบฟิลด์ `{0}`',
+    'forEmptyInputGiven'               => 'มีการระบุคำสั่งที่ว่างเปล่าสำหรับฟิลด์ `{0}`',
+    'forFindColumnHaveMultipleColumns' => 'อนุญาตให้ระบุเพียงคอลัมน์เดียวในชื่อคอลัมน์',
+];
