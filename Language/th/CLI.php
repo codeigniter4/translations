b2dacd1ad9fec21892755100ed0286323d72b180
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-09 14:49:04 +0700

    Translated cli.php to Thai

---
+++ Language/th/CLI.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// CLI language settings
+return [
+    // 'altCommandPlural'   => 'Did you mean one of these?',
+    'altCommandPlural'   => 'คุณหมายถึงสิ่งหนึ่งในเหล่านี้ใช่ไหม',
+    'altCommandSingular' => 'คุณหมายถึงสิ่งนี้ใช่ไหม',
+    'commandNotFound'    => 'ไม่พบคำสั่ง "{0}"',
+    'generator'          => [
+        'cancelOperation' => 'การดำเนินการถูกยกเลิก',
+        'className'       => [
+            'command'    => 'ชื่อคลาสคำสั่ง',
+            'config'     => 'ชื่อคลาสคอนฟิก',
+            'controller' => 'ชื่อคลาสคอนโทรลเลอร์',
+            'default'    => 'ชื่อคลาส',
+            'entity'     => 'ชื่อคลาสเอนทิตี',
+            'filter'     => 'ชื่อคลาสฟิลเตอร์',
+            'migration'  => 'ชื่อคลาสการย้ายข้อมูล',
+            'model'      => 'ชื่อคลาสโมเดล',
+            'seeder'     => 'ชื่อคลาสแจกจ่ายข้อมูล',
+            'validation' => 'ชื่อคลาสการตรวจสอบ',
+        ],
+        'commandType'      => 'ประเภทคำสั่ง',
+        'databaseGroup'    => 'กลุ่มฐานข้อมูล',
+        'fileCreate'       => 'สร้างไฟล์แล้ว: {0}',
+        'fileError'        => 'เกิดข้อผิดพลาดขณะสร้างไฟล์: {0}',
+        'fileExist'        => 'มีไฟล์อยู่แล้ว: {0}',
+        'fileOverwrite'    => 'ไฟล์ถูกเขียนทับ: {0}',
+        'parentClass'      => 'คลาสหลัก',
+        'returnType'       => 'ประเภทการส่งคืน',
+        'tableName'        => 'ชื่อตาราง',
+        'usingCINamespace' => 'คำเตือน: การใช้เนมสเปซ "CodeIgniter" จะสร้างไฟล์ในไดเร็กทอรีระบบ',
+    ],
+    'helpArguments'       => 'อาร์กิวเม้นต์:',
+    'helpDescription'     => 'คำอธิบาย:',
+    'helpOptions'         => 'ตัวเลือก:',
+    'helpUsage'           => 'การใช้งาน:',
+    'invalidColor'        => 'สิ {0} ไม่ถูกต้อง: {1}',
+    'namespaceNotDefined' => 'เนมสเปซ "{0}" ไม่ได้ถูกนิยามไว้',
+];
