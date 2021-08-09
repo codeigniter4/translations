8b0fc666457d2cb78f50b480db69e8b49f0e581c
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-09 20:13:40 +0700

    Translated to Thai

---
+++ Language/th/Core.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Core language settings
+return [
+    'copyError'                    => 'พบข้อผิดพลาดขณะพยายามแทนที่ไฟล์ ({0}) โปรดตรวจสอบให้แน่ใจว่าไดเร็กทอรีไฟล์ของคุณสามารถเขียนได้',
+    'enabledZlibOutputCompression' => 'คำสั่ง ini "zlib.output_compression" ของคุณเปิดอยู่ สิ่งนี้จะทำงานได้ไม่ดีนักกับบัฟเฟอร์เอาต์พุต',
+    'invalidFile'                  => 'ไฟล์ไม่ถูกต้อง: {0}',
+    'invalidPhpVersion'            => 'เวอร์ชัน PHP ของคุณต้องเป็น {0} หรือสูงกว่าเพื่อเรียกใช้ CodeIgniter เวอร์ชันปัจจุบัน: {1}',
+    'missingExtension'             => 'เฟรมเวิร์กจำเป็นต้องติดตั้งและโหลดส่วนขยายต่อไปนี้: {0}',
+    'noHandlers'                   => '{0} ต้องระบุ handler จัดการอย่างน้อยหนึ่งตัว',
+];
