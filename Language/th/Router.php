d6b4256f9cc8e91625a605f5e27f2e73c784478c
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-14 21:08:20 +0700

    Translated to Thai

---
+++ Language/th/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Router language settings
+return [
+    'invalidParameter'    => 'พารามิเตอร์ไม่ตรงกับประเภทที่คาดไว้',
+    'missingDefaultRoute' => 'ไม่สามารถระบุได้ว่าควรแสดงอะไร ไม่ได้ระบุเส้นทางเริ่มต้นในไฟล์เส้นทาง',
+];
