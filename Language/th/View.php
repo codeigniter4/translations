4cae48d18d378104c965e978dad363513521942c
Anuchit Thiamuan <thiam.uan3@gmail.com>
2021-08-14 21:14:54 +0700

    Translated thai

---
+++ Language/th/View.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// View language settings
+return [
+    'invalidCellMethod'     => '{class}::{method} เป็นเมธอดที่ไม่ถูกต้อง',
+    'missingCellParameters' => '{class}::{method} ไม่มีพารามิเตอร์',
+    'invalidCellParameter'  => '{0} ไม่ใช่ชื่อพารามิเตอร์ที่ถูกต้อง',
+    'noCellClass'           => 'ไม่มีการกำหนดคลาสเซลล์วิว',
+    'invalidCellClass'      => 'ไม่พบคลาสเซลล์วิว: {0}',
+    'tagSyntaxError'        => 'คุณมีข้อผิดพลาดทางไวยากรณ์ในแท็ก Parser ของคุณ: {0}',
+];
