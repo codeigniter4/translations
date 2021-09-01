dc2f84b25c2799dbc29ea762170cd9d3c3c8a8c0
Eyad Bereh <eyad@ixcoders.com>
2021-09-02 00:32:34 +0300

    Added missing entries

--- Language/ar/CLI.php
+++ Language/ar/CLI.php
@@ -11,10 +11,27 @@
 
 // CLI language settings
 return [
-	'commandNotFound' => 'الأمر "{0}" غير موجود.',
-	'helpUsage'       => 'الاستخدام:',
-	'helpDescription' => 'الوصف:',
-	'helpOptions'     => 'الخيارات:',
-	'helpArguments'   => 'الوسائط:',
-	'invalidColor'    => 'اللون {0} غير صالح: {1}.',
+	'altCommandPlural'   => 'هل كنت تقصد إحدى الأوامر التالية ؟',
+	'altCommandSingular' => 'هل كنت تقصد هذا ؟',
+	'commandNotFound'    => 'الأمر "{0}" غير موجود.',
+	'generator'          => [
+		'cancelOperation'  => 'تم إلغاء العملية.',
+		'className'        => 'اسم الصنف',
+		'commandType'      => 'نوع الأمر',
+		'databaseGroup'    => 'مجموعة قاعدة البيانات',
+		'fileCreate'       => 'تم إنشاء الملف: {0}',
+		'fileError'        => 'حدث خطأ أثناء إنشاء الملف: {0}',
+		'fileExist'        => 'الملف موجود مسبقاً: {0}',
+		'fileOverwrite'    => 'تمت الكتابة فوق الملف: {0}',
+		'parentClass'      => 'الصنف الأب',
+		'returnType'       => 'النوع المرجع',
+		'tableName'        => 'اسم الجدول',
+		'usingCINamespace' => 'تحذير: إن إستخدام فضاء الأسماء "CodeIgniter" سوف يؤدي إلى توليد الملف في مسار system.',
+	],
+	'helpArguments'       => 'الوسائط:',
+	'helpDescription'     => 'الوصف:',
+	'helpOptions'         => 'الخيارات:',
+	'helpUsage'           => 'الاستخدام:',
+	'invalidColor'        => 'اللون {0} غير صالح: {1}.',
+	'namespaceNotDefined' => 'إن فضاء الأسماء "{0}" غير معرف.',
 ];
