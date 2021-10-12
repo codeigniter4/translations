fcb21d5abf1af7c9f6162e120c067d20102c32f6
임동덕 <cycity@nate.com>
2021-10-12 15:17:23 +0900

    Update ko lang

--- Language/ko/Files.php
+++ Language/ko/Files.php
@@ -11,6 +11,8 @@
 
 // Files language settings
 return [
-    'fileNotFound' => '파일을 찾을 수 없습니다: {0}', // 'File not found: {0}',
-    'cannotMove'   => '파일 {0} 경우에 {1} ({2}) 쪽으로 이동할 수 없습니다.', // 'Could not move file {0} to {1} ({2})',
+    'fileNotFound'      => '파일을 찾을 수 없습니다: {0}', // 'File not found: {0}',
+    'cannotMove'        => '파일 {0} 경우에 {1} ({2}) 쪽으로 이동할 수 없습니다.', // 'Could not move file {0} to {1} ({2})',
+    'expectedDirectory' => '{0}에 유효한 디렉토리가 없습니다.', // '{0} expects a valid directory.',
+    'expectedFile'      => '{0}에 유효한 파일이 없습니다.', // '{0} expects a valid file.',
 ];
