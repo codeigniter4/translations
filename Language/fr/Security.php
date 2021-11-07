363fdb8cecc2bfd79549bcef40d1d3ba9b75ac93
Alain Rivest <includebeer@gmail.com>
2021-11-07 15:41:11 -0500

    update missing french translation

---
+++ Language/fr/Security.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Security language settings
+return [
+    'disallowedAction' => 'L\'action que vous avez demandée n\'est pas autorisée.',
+
+    // @deprecated
+    'invalidSameSite' => 'La valeur SameSite doit être None, Lax, Strict ou une chaîne vide. Donnée : {0}',
+];
