2aa9a5373ac09c0a4eb47f89f6f5151f41ff98cc
Toto Prayogo <totoprayogo1916@gmail.com>
2021-11-13 19:25:21 +0700

    update id/Cast.php, add id/Publisher.php & id/Test.php

---
+++ Language/id/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => 'Penerbit mengalami {0} yang tidak terduga saat menyalin {1} ke {2}.',
+    'destinationNotAllowed' => 'Tujuan tidak ada dalam daftar direktori Penerbit yang diizinkan: {0}',
+    'fileNotAllowed'        => '{0} gagal dalam pembatasan berikut untuk {1}: {2}',
+
+    // Publish Command
+    'publishMissing' => 'Tidak ada kelas Penayang yang terdeteksi di {0} di semua ruang nama.',
+    'publishSuccess' => '{0} memublikasikan {1} berkas ke {2}.',
+    'publishFailure' => '{0} gagal memublikasikan ke {1}!',
+];
