6ca8e76dfd6834b715a2f3ca101fe95c7e2d6d38
Alex Schmitz <40514119+sfadschm@users.noreply.github.com>
2021-11-17 14:14:30 +0100

    Fix missing DE keys. (#257)

---
+++ Language/de/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => 'Publisher hat ein unerwartetes {0} beim Kopieren von {1} nach {2} festgestellt.',
+    'destinationNotAllowed' => 'Das Ziel befindet sich nicht in der Liste der zulässigen Publisher-Verzeichnisse: {0}',
+    'fileNotAllowed'        => '{0} erfüllt die folgende Einschränkung für {1} nicht: {2}',
+
+    // Publish Command
+    'publishMissing' => 'Es konnten keine Publisher-Klassen in {0} in allen Namensräumen detektiert werden.',
+    'publishSuccess' => '{0} hat {1} Datei(en) nach {2} publiziert.',
+    'publishFailure' => '{0} konnte nicht nach {1} publiziert werden!',
+];
