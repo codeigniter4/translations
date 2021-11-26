2da322b606e18018efcb1dc811d9a8a1583a55dd
windpie1 <wzf28@sina.com>
2021-11-26 20:40:23 +0800

    add missing translation(zh-CN/Time.php)

--- Language/zh-CN/Time.php
+++ Language/zh-CN/Time.php
@@ -11,12 +11,20 @@
 
 // Time language settings
 return [
+    'invalidFormat'  => '"{0}" 不是有效的 datetime 格式。',
     'invalidMonth'   => '月份必须在 1 至 12 之间。 现在是: {0}',
     'invalidDay'     => '天数必须在 1 至 31 之间。 现在是: {0}',
     'invalidOverDay' => '天数必须在 1 至 {0} 之间。 现在是: {1}',
     'invalidHours'   => '小时必须在 0 至 23 之间。 现在是: {0}',
     'invalidMinutes' => '分钟必须在 0 至 59 之间。 现在是: {0}',
     'invalidSeconds' => '秒数必须在 0 至 59 之间。 现在是: {0}',
+    'years'          => '{0, plural, =1{# 年} other{# 年}}',
+    'months'         => '{0, plural, =1{# 月} other{# 月}}',
+    'weeks'          => '{0, plural, =1{# 周} other{# 周}}',
+    'days'           => '{0, plural, =1{# 天} other{# 天}}',
+    'hours'          => '{0, plural, =1{# 小时} other{# 小时}}',
+    'minutes'        => '{0, plural, =1{# 分钟} other{# 分钟}}',
+    'seconds'        => '{0, plural, =1{# 秒钟} other{# 秒钟}}',
     'ago'            => '{0} 以前',
     'inFuture'       => '在 {0}',
     'yesterday'      => '昨天',
