edf2c89acc6ad4d40e9d449b213541b33a724cfd
Mattias Sandstrom <1013635+tangix@users.noreply.github.com>
2021-11-30 22:13:35 +0100

    Updated translations for sv-SE

---
+++ Language/sv-SE/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => 'Publisher hittade en oväntad {0} under kopieringen av {1} till {2}.',
+    'destinationNotAllowed' => 'Målkatalogen är inte på den tillåtna listan av Publisher kataloger: {0}',
+    'fileNotAllowed'        => '{0} uppfyller inte förutsättning för {1}: {2}',
+
+    // Publish Command
+    'publishMissing' => 'Inga Publisher-klasser kunde hittas i {0} under något namespace.',
+    'publishSuccess' => '{0} publicerade {1} fil(er) till {2}.',
+    'publishFailure' => '{0} kunde inte publicera till {1}!',
+];
