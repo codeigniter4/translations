cacd21bd009f11b97ccc4810e6f365ae1cfcce0b
Danilo Giorgi <danygiorgi@hotmail.com>
2021-12-04 16:05:10 +0100

    Create Publisher.php

---
+++ Language/it/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => 'Publisher ha riscontrato un imprevisto {0} mentre copia {1} su {2}.',
+    'destinationNotAllowed' => 'La destinazione non è nell\'elenco autorizzato delle directory di Publisher: {0}',
+    'fileNotAllowed'        => '{0} fallisce la seguente restrizione per {1}: {2}',
+
+    // Publish Command
+    'publishMissing' => 'Nessuna classe di Publisher rilevata in {0} attraverso tutti i namespaces.',
+    'publishSuccess' => '{0} pubblicati {1} file(s) di {2}.',
+    'publishFailure' => '{0} non è riuscito a pubblicare {1}!',
+];
