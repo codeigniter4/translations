ff1eb23bedd0faee23fa1a0f914d307e2308e12c
demyr17 <61029105+demyr17@users.noreply.github.com>
2021-12-07 22:47:16 +0300

    Create Publisher.php

---
+++ Language/tr/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => '{1}\'i {2}\'ye kopyalarken Publisher beklenmedik {0} ile karşılaştı.',
+    'destinationNotAllowed' => 'Hedef, ilgili Publisher yönergelerindeki izinli listesinde bulunmuyor : {0}',
+    'fileNotAllowed'        => '{0}, belirtilen {1}: {2} kısıtlaması konusunda başarısız oldu',
+
+    // Publish Command
+    'publishMissing' => 'Tüm namespacesler üzerinde {0}\'da hiçbir Publisher sınıfı tespit edilmedi.',
+    'publishSuccess' => '{0}, {1} dosyayı {2}\'ye yayınladı.',
+    'publishFailure' => '{0}, {1}\'e yayınlama konusunda başarısız oldu!',
+];
