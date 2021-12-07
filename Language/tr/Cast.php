5890a7c53b2712a2a998b5284e593cf70fd67b17
demyr17 <61029105+demyr17@users.noreply.github.com>
2021-12-07 14:01:40 +0300

    Create Cast.php

---
+++ Language/tr/Cast.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cast language settings
+return [
+    'baseCastMissing'        => '"{0}" sınıfı "CodeIgniter\Entity\Cast\BaseCast" sınıfını miras almalı.',
+    'invalidCastMethod'      => '"{0}" geçersiz bir cast metodu, geçerli metodlar: ["get", "set"].',
+    'invalidTimestamp'       => 'Geçersiz TimeStamp. "timestamp" tip castingi doğru bir format gerektirir.',
+    'jsonErrorCtrlChar'      => 'Beklenmedik kontrol karakteri bulundu.',
+    'jsonErrorDepth'         => 'Maksimum yığın derinliği (stack depth) aşıldı.',
+    'jsonErrorStateMismatch' => 'JSON hata durumu uyuşmazlığı. Yetersiz akış ya da mod uyuşmazlığı.',
+    'jsonErrorSyntax'        => 'Syntax hatası, JSON doğru oluşturulmadı.',
+    'jsonErrorUnknown'       => 'Bilinmeyen hata.',
+    'jsonErrorUtf8'          => 'Kötü biçimlendirilmiş UTF-8 karakterleri, muhtemelen yanlış encode edildi.',
+];
