f98aa1415993f56a21d52c1e7e84f43898b9e60f
vibbow <vibbow@hotmail.com>
2021-12-27 07:25:00 +0800

    [zh-TW] Update translations

---
+++ Language/zh-TW/Publisher.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+    'collision'             => '分發器在 將{1} 複製 到{2} 時遇到意外 {0}。',
+    'destinationNotAllowed' => '目標不在分發器目錄的允許清單中：{0}',
+    'fileNotAllowed'        => '{0}未通過以下限制 {1}: {2}{0}。',
+
+    // Publish Command
+    'publishMissing' => '在所有命名空間中未檢測到 {0} 分發器類。',
+    'publishSuccess' => '{0} 已發 佈{1} 檔 到{2}。',
+    'publishFailure' => '{0} 未能發佈到 {1}！',
+];
