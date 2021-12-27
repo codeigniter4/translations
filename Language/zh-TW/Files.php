6071119730e0717176e7886d1a275740c287724a
vibbow <vibbow@hotmail.com>
2021-12-27 10:17:58 +0800

    [zh-TW] Update translations (#276)
    * [zh-TW] Update translations
    
    * Delete invisiable char.
    
    * Fix code align

--- Language/zh-TW/Files.php
+++ Language/zh-TW/Files.php
@@ -11,6 +11,8 @@
 
 // Files language settings
 return [
-    'fileNotFound' => '找不到檔案：{0}',
-    'cannotMove'   => '無法將檔案從 {0} 移動到 {1}。({2})',
+    'fileNotFound'      => '找不到檔案：{0}',
+    'cannotMove'        => '無法將檔案從 {0} 移動到 {1}。({2})',
+    'expectedDirectory' => '{0} 需要是一個有效的目錄。',
+    'expectedFile'      => '{0} 需要一個有效的檔。',
 ];
