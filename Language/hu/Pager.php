cde8b5284857fdaed36a6986c92b368eaf1f1a21
Zoltan Kovacs <kzolee1@gmail.com>
2022-02-03 20:51:26 +0100

    Another 7 files translated

---
+++ Language/hu/Pager.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Pager language settings
+return [
+    'pageNavigation'         => 'Oldal navigáció',
+    'first'                  => 'Első',
+    'previous'               => 'Előző',
+    'next'                   => 'Következő',
+    'last'                   => 'Utolsó',
+    'older'                  => 'Régebbi',
+    'newer'                  => 'Újabb',
+    'invalidTemplate'        => '{0} nem érvényes Pager sablon.',
+    'invalidPaginationGroup' => '{0} nem érvényes oldalszámozási csoport.',
+];
---
+++ Language/hu/Security.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Security language settings
+return [
+    'disallowedAction' => 'A kért művelet nem hajtható végre.',
+
+    // @deprecated
+    'invalidSameSite' => 'Munkamenet: A SameSite beállításnak None, Lax, Strict vagy üres karakterláncnak kellene lennie. Megadva: {0}',
+];
