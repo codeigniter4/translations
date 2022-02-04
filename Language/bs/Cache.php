302b06517396c9a5a6fcdf52bb30a48821af8d30
demirkaric <demirkaric@hotmail.com>
2022-02-04 19:35:31 +0100

    Cache translation

---
+++ Language/bs/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cache language settings
+return [
+    'unableToWrite'   => 'Nemoguć upis keša na {0}.',
+    'invalidHandlers' => 'Keš konfig mora sadržavati niz od $validHandlers.',
+    'noBackup'        => 'Keš konfig mora imati postavljen handler i backupHandler.',
+    'handlerNotFound' => 'Keš konfig ima nevalidan handler ili backup handler.',
+];
