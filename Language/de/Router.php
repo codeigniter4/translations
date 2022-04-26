145e27763a6cf0dca9f745e8ef805a8e00abf1a3
Stefan Bauer <sba@gmx.ch>
2022-04-26 10:07:34 +0200

    Updating de with latest strings (Fix mistakenly moved comment)

--- Language/de/Router.php
+++ Language/de/Router.php
@@ -9,10 +9,10 @@
  * the LICENSE file that was distributed with this source code.
  */
 
+// Router language settings
 return [
     'invalidParameter'         => 'Ein Parameter stimmt nicht mit dem erwarteten Typ überein.',
     'missingDefaultRoute'      => 'Es konnte nicht bestimmt werden, was angezeigt werden soll. In der Routingdatei wurde keine Standardroute angegeben.',
     'invalidDynamicController' => 'Ein dynamischer Kontroller ist aus Sicherheitsgründen nicht erlaubt. Route handler: {0}',
     'invalidControllerName'    => 'Das Namensraumbegrenzungszeichen ist ein Backslash (\), kein Schrägstrich (/). Route handler: {0}',
 ];
-// Router language settings
