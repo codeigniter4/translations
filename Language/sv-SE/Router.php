9446b84a13ff9bb752a7f9b879724b24d30c3c07
Mattias Sandström <1013635+tangix@users.noreply.github.com>
2022-06-03 10:54:11 +0200

    Updated sv-SE with new strings. (#303)

--- Language/sv-SE/Router.php
+++ Language/sv-SE/Router.php
@@ -13,4 +13,5 @@ return [
     'invalidParameter'         => 'En parameter är inte av den förväntade typen.',
     'missingDefaultRoute'      => 'Kan inte avgöra vad som skall visas. En förvald route har inte angivits i route-filen.',
     'invalidDynamicController' => 'En dynamisk controller är inte tillåten av säkerhetsskäl. Route hanterare: {0}',
+    'invalidControllerName'    => 'Namnet på namespace delas med backslash (\), inte slash (/). Route hanterare: {0}',
 ];
