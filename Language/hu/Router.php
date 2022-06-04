14fc2b84d3b2479720d8d151d998b6cac2368dbf
Ed <ed@gmail.com>
2022-06-04 18:26:30 +0300

    Router.php add missing param [HU]

--- Language/hu/Router.php
+++ Language/hu/Router.php
@@ -11,8 +11,8 @@
 
 // Router language settings
 return [
-    'invalidParameter'    => 'Egy paraméter nem felel meg a várt típusnak.',
-    'missingDefaultRoute' => 'Nincs meghatározva, hogy mit kellene megjeleníteni. Az alap útvonal nincs meghatározva az útvonalleíró fájlban.',
+    'invalidParameter'         => 'Egy paraméter nem felel meg a várt típusnak.',
+    'missingDefaultRoute'      => 'Nincs meghatározva, hogy mit kellene megjeleníteni. Az alap útvonal nincs meghatározva az útvonalleíró fájlban.',
     'invalidDynamicController' => 'Biztonsági okokból nem engedélyezett a dinamikus vezérlő. Útvonalkezelő: {0}',
     'invalidControllerName'    => 'A névtér határolója fordított perjel (\), nem pedig perjel (/). Útvonalkezelő: {0}',
 ];
