d1140c482f2a9988ca8e2988c88980d936653ed9
Mohammad Salehi <mds1121@yahoo.com>
2022-08-27 15:23:36 +0430

    [fa] add missing keys

--- Language/fa/View.php
+++ Language/fa/View.php
@@ -14,4 +14,7 @@ return [
     'invalidCellMethod'     => '{class}::{method} تابع معتبری نیست.',
     'missingCellParameters' => '{class}::{method} پارامتری ندارد.',
     'invalidCellParameter'  => '{0} نام پارامتر معتبری نیست.',
-];
+    'noCellClass'           => 'هیچ کلاس view cell مشخص نشده.',
+    'invalidCellClass'      => 'کلاس view cell با نام {0} پیدا نشد.',
+    'tagSyntaxError'        => 'یک خطای گرامی در Parser tags وجود دارد: {0}',
+    'invalidDecoratorClass' => '{0} یک View Decorator معتبر نیست.',];
