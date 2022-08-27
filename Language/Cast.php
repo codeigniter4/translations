2069e86b166eff1b6e2ecfca8a30d5e1c16f5bf5
Mohammad Salehi <mds1121@yahoo.com>
2022-08-27 10:43:02 +0430

    Create Cast.php

---
+++ Language/Cast.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cast language settings
+return [
+    'baseCastMissing'        => 'کلاس "{0}" باید از کلاس "CodeIgniter\Entity\Cast\BaseCast" ارث بری کند.',
+    'invalidCastMethod'      => 'The "{0}" is invalid cast method, valid methods are: ["get", "set"].',
+    'invalidTimestamp'       => 'نوع casting در "timestamp" باید یک timestamp معتبر باشد.',
+    'jsonErrorCtrlChar'      => 'Unexpected control غیر منتظره پیدا شد.',
+    'jsonErrorDepth'         => 'Maximum stack depth exceeded.',
+    'jsonErrorStateMismatch' => 'Underflow or the modes mismatch.',
+    'jsonErrorSyntax'        => 'Syntax error, malformed JSON.',
+    'jsonErrorUnknown'       => 'خطای نامشخص.',
+    'jsonErrorUtf8'          => 'Malformed UTF-8 characters, possibly incorrectly encoded.',
+];
