5cc9178289667a9cb38f3b9a9bc71258f71725e5
Mohammad Salehi <mds1121@yahoo.com>
2022-08-27 11:25:33 +0430

    Create CLI.php

---
+++ Language/fa/CLI.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// CLI language settings
+return [
+    'altCommandPlural'   => 'آیا منظورتان یکی از این ها بود؟',
+    'altCommandSingular' => 'آیا منظورتان این بود؟',
+    'commandNotFound'    => 'دستور "{0}" پیدا نشد.',
+    'generator'          => [
+        'cancelOperation' => 'عملیات لغو شد.',
+        'className'       => [
+            'command'    => 'نام کلاس Command',
+            'config'     => 'نام کلاس Config',
+            'controller' => 'نام کلاس Controller',
+            'default'    => 'نام کلاس',
+            'entity'     => 'نام کلاس Entity',
+            'filter'     => 'نام کلاس Filter',
+            'migration'  => 'نام کلاس Migration',
+            'model'      => 'نام کلاس Model',
+            'seeder'     => 'نام کلاس Seeder',
+            'validation' => 'نام کلاس Validation',
+        ],
+        'commandType'      => 'نوع Command',
+        'databaseGroup'    => 'گروه دیتابیس',
+        'fileCreate'       => 'فایل ایجاد شد: {0}',
+        'fileError'        => 'خطا در هنگام ایجاد فایل: {0}',
+        'fileExist'        => 'فایل از قبل وجود دارد: {0}',
+        'fileOverwrite'    => 'فایل رونویسی شد: {0}',
+        'parentClass'      => 'کلاس والد',
+        'returnType'       => 'Return type',
+        'tableName'        => 'نام جدول',
+        'usingCINamespace' => 'هشدار: استفاده از فضای نام "CodeIgniter" باعث ایجاد فایل در دایرکتوری system می‌شود .',
+    ],
+    'helpArguments'       => 'پارامتر های ورودی:',
+    'helpDescription'     => 'توضیحات:',
+    'helpOptions'         => 'آپشن ها:',
+    'helpUsage'           => 'نحوه استفاده:',
+    'invalidColor'        => 'رنگ {0} نامعتبر: {1}.',
+    'namespaceNotDefined' => 'فضای نام "{0}" تعریف نشده است.',
+];
