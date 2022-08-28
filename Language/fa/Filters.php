32c35c7766932762b092b1b8c0ab4e509805a0a5
Mohammad Salehi <mds1121@yahoo.com>
2022-08-28 16:24:07 +0430

    add missing files

---
+++ Language/fa/Filters.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Filters language settings
+return [
+    'noFilter'           => 'برای فیلتر {0} باید alias منطبق تعریف شده باشد.',
+    'incorrectInterface' => '{0} باید بر اساس اینترفیس CodeIgniter\Filters\FilterInterface پیاده سازی شده باشد.',
+];
---
+++ Language/fa/Security.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Security language settings
+return [
+    'disallowedAction' => 'عملیات درخواستی شما مجاز نیست.',
+
+    // @deprecated
+    'invalidSameSite' => 'مقدار SameSite باید یکی از مقادیر None, Lax, Strict یا یک رشته‌ی خالی باشد، مقدار داده شده: {0}',
+];
