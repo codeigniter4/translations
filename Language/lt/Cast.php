38422f7200a433b84941f714e3828e20f372d9df
Donatas Glodenis <dgvirtual@akl.lt>
2022-09-17 14:34:26 +0300

    updates and corrections to pass tests

--- Language/lt/Cast.php
+++ Language/lt/Cast.php
@@ -14,10 +14,10 @@ return [
     'baseCastMissing'        => '"{0}" turi paveldėti "CodeIgniter\Entity\Cast\BaseCast" klasę.',
     'invalidCastMethod'      => '"{0}" yra neteisingas cast metodas, teisingi metodai yra: ["get", "set"].',
     'invalidTimestamp'       => 'Tipas "timestamp" teisingos laiko žymos.',
+    'jsonErrorCtrlChar'      => 'Aptiktas nelauktas kontrolės simbolis',
     'jsonErrorDepth'         => 'Viršytas maksimalus dėklo gylis',
     'jsonErrorStateMismatch' => 'Nepakankama tėkmė arba režimų neatitikimas',
-    'jsonErrorCtrlChar'      => 'Aptiktas nelauktas kontrolės simbolis',
     'jsonErrorSyntax'        => 'Sintaksės klaida, netesingai suformuotas JSON',
-    'jsonErrorUtf8'          => 'Neteisingi UTF-8 simboliai, JSON gali būti neteisingai užkoduotas',
     'jsonErrorUnknown'       => 'Nežinoma klaida',
+    'jsonErrorUtf8'          => 'Neteisingi UTF-8 simboliai, JSON gali būti neteisingai užkoduotas',
 ];
