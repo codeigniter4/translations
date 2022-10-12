376eb71cc875c142518d9d1c426d2cee16fd59d1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-10-13 03:45:24 +0600

    cache bangla translation added

--- Language/bn/Cache.php
+++ Language/bn/Cache.php
@@ -11,8 +11,8 @@
 
 // Cache language settings
 return [
-    'unableToWrite'   => 'Cache unable to write to {0}.',
-    'invalidHandlers' => 'Cache config must have an array of $validHandlers.',
-    'noBackup'        => 'Cache config must have a handler and backupHandler set.',
-    'handlerNotFound' => 'Cache config has an invalid handler or backup handler specified.',
+    'unableToWrite'   => '{0} এ ক্যাশ লিখতে অক্ষম।',
+    'invalidHandlers' => 'ক্যাশ কনফিগারেশনে অবশ্যই $validHandlers এর একটি অ্যারে থাকতে হবে।',
+    'noBackup'        => 'ক্যাশ কনফিগারে অবশ্যই একটি হ্যান্ডলার এবং ব্যাকআপহ্যান্ডলার সেট থাকতে হবে।',
+    'handlerNotFound' => 'ক্যাশ কনফিগারেশনে একটি অবৈধ হ্যান্ডলার বা ব্যাকআপ হ্যান্ডলার নির্দিষ্ট করা আছে।',
 ];
