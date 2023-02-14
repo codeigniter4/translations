04091a7ecad1253a08efdbeea5b375d5c4e3e0e0
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2023-02-14 09:10:22 +0330

    add `Errors.php` for 4.3

---
+++ Language/fa/Errors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+return [
+    'pageNotFound'    => '404 - صفحه پیدا نشد',
+    'sorryCannotFind' => 'متاسفیم! به نظر نمی رسد صفحه ای که به دنبال آن هستید را پیدا کنید.',
+    'whoops'          => 'اوو!',
+    'weHitASnag'      => 'به نظر میرسد مشکلی وجود دارد. لطفا مجددا بعدا تلاش کنید ...',
+];
