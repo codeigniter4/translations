8f179edad814353f29c322f62bd6c5b8d59a5599
MalkoVrinto <MalkoVrinto@gmail.com>
2023-03-01 15:50:56 +0100

    Added Errors.php ru_RU
    added russian translation of Errors.php

---
+++ Language/ru/Errors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+return [
+    'pageNotFound'    => '404 - Страница не найдена',
+    'sorryCannotFind' => 'Сожалеем! Не удаётся найти страницу, которую вы искали.',
+    'whoops'          => 'Ой!',
+    'weHitASnag'      => 'Кажется, мы столкнулись с препятствием. Пожалуйста, повторите попытку позже...',
+];
