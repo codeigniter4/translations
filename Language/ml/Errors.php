de4476c46f36f9b8a7b8b2af186765571c4f74bb
Rakhesh Thayyur <rakheshthayyur@gmail.com>
2023-03-17 12:06:40 +0530

    Malayalam language missing files added

---
+++ Language/ml/Errors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+return [
+    'pageNotFound'    => '৪০৪ - പേജ് കണ്ടെത്തിയില്ല',
+    'sorryCannotFind' => 'ക്ഷമിക്കണം!നിങ്ങൾ തിരയുന്ന പേജ് കണ്ടെത്താനായില്ല. ',
+    'whoops'          => 'ക്ഷമിക്കണം! ',
+    'weHitASnag'      => 'ഒരു പ്രശ്നമുണ്ടെന്ന് ഞങ്ങൾ കരുതുന്നു.പിന്നീട് വീണ്ടും ശ്രമിക്കുക ... ',
+];
---
+++ Language/ml/Security.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Security language settings
+return [
+    'disallowedAction' => 'നിങ്ങൾ അഭ്യർത്ഥിച്ച പ്രവർത്തനം അനുവദനീയമല്ല.',
+
+    // @deprecated
+    'invalidSameSite' => 'SameSite മൂല്യം തീർച്ചയായും None, Lax, Stഅല്ലെങ്കിൽ ഒരു ശൂന്യമായ സ്ട്രിംഗ് ആയിരിക്കണം.നൽകിയിട്ടുണ്ട്: {0}',
+];
