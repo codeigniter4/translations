4b1bb7579afa06a32ea59cc792a078c810588551
Rakhesh Thayyur <rakheshthayyur@gmail.com>
2023-03-17 12:24:32 +0530

    Malayalam language missing files added as per the status from https://codeigniter4.github.io/translations/

--- Language/ml/Log.php
+++ Language/ml/Log.php
@@ -11,6 +11,6 @@
 
 // Log language settings
 return [
-    'invalidLogLevel' => '{0} ഒരു അസാധുവായ ലോഗ് നിലയമാണ്.',
+    'invalidLogLevel'    => '{0} ഒരു അസാധുവായ ലോഗ് നിലയമാണ്',
     'invalidMessageType' => 'നൽകിയ സന്ദേശ തരം "{0}" പിന്തുണയ്ക്കുന്നില്ല.',
 ];
