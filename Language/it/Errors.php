14b4ed96928f8aca795690164bb84eb43d2b9530
Zio3D <42025157+Zio3D@users.noreply.github.com>
2023-08-03 11:29:00 +0200

    Add files via upload

---
+++ Language/it/Errors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+return [
+    'pageNotFound'    => '404 - pagina non trovata',
+    'sorryCannotFind' => 'Spiacente! Non riesco a trovare la pagina che stavi cercando.',
+    'whoops'          => 'Ops!',
+    'weHitASnag'      => 'Sembra che abbiamo incontrato un intoppo. Per favore riprova più tardi...',
+];
