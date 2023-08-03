c7d85ac473f60ce462fa4f72ca94749ff0829e54
Zio3D <42025157+Zio3D@users.noreply.github.com>
2023-08-03 14:27:27 +0200

    Update Router.php

--- Language/it/Router.php
+++ Language/it/Router.php
@@ -14,5 +14,5 @@ return [
     'invalidParameter'         => 'Un parametro non rientra nella tipologia attesa.',
     'missingDefaultRoute'      => 'Impossibile determinare cosa si dovrebbe mostrare. Non è stato specificato un percorso di default nel file percorsi (routing).',
     'invalidDynamicController' => 'Un controller dinamico non è consentito per motivi di sicurezza. Gestore dei percorsi: "{0}"',
-    'invalidControllerName'    => 'The namespace delimiter is a backslash (\), not a slash (/). Gestore dei percorsi: "{0}"',
+    'invalidControllerName'    => 'Il delimitatore dello spazio dei nomi è una barra rovesciata (\), non una barra (/). Gestore dei percorsi: "{0}"',
 ];
