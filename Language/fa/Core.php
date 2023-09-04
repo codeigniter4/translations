c87f4eedb3e8e020bcb180564ad8bf92627c41ff
Pooya Parsa <pooya_parsa_dadashi@yahoo.com>
2023-09-04 19:56:21 +0330

    lang: [fa] update core.php

--- Language/fa/Core.php
+++ Language/fa/Core.php
@@ -14,6 +14,7 @@ return [
     'copyError'                    => 'مشکلی در هنگام جایگزینی فایل بوجود آمده است. لطفا اطمینان حاصل کنید که شاخه‌ی فایل قابل نوشتن است.',
     'enabledZlibOutputCompression' => 'تنظیمات zlib.output_compression در ini فعال است، این مورد با خروجی بافر به درستی کار نمی‌کند.',
     'invalidFile'                  => 'فایل نامعتبر: {0}',
+    'invalidDirectory'             => 'دایرکتوری وجود ندارد: "{0}"',
     'invalidPhpVersion'            => 'برای اجرای Codeigniter حداقل نسخه‌ی PHP باید {0} باشد. نسخه‌ی فعلی: {1}',
     'missingExtension'             => 'افزونه‌ی {0} بارگزاری نشده است.',
     'noHandlers'                   => '{0} باید حداقل یک Handler داشته باشد.',
