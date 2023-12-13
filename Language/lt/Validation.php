7e655fb6127535967ef906b3114851baf748552b
Donatas Glodenis <dgvirtual@akl.lt>
2023-12-13 21:47:51 +0200

    Add code style fixes

--- Language/lt/Validation.php
+++ Language/lt/Validation.php
@@ -19,16 +19,16 @@ return [
     'invalidTemplate' => '{0} nėra tinkamas validavimo šablonas.',
 
     // Rule Messages
-    'alpha'                 => 'Lauke „{field}“ gali būti tik abėcėlės raidės.',
-    'alpha_dash'            => 'Lauke „{field}“ gali būti tik raidės, skaičiai, brūkšneliai ir apatiniai brūkšneliai.',
-    'alpha_numeric'         => 'Lauke „{field}“ gali būti tik raidės ir skaičiai.',
-    'alpha_numeric_punct'   => 'Lauke „{field}“ gali būti tik skaičiai, raidės, tarpeliai, ir  ~ ! # $ % & * - _ + = | : . simboliai.',
-    'alpha_numeric_space'   => 'Lauke „{field}“ gali būti tik raidės, skaičiai ir tarpai.',
-    'alpha_space'           => 'Lauke „{field}“ gali būti tik raidės ir tarpai.',
-    'decimal'               => 'Lauke „{field}“ turi būti dešimtainis skaičius.',
-    'differs'               => 'Laukas „{field}“ turi skirtis nuo „{param}“ lauko.',
-    'equals'                => 'Laukas „{field}“ turi tiksliai atitikti: {param}.',
-    'exact_length'          => '{param, plural,
+    'alpha'               => 'Lauke „{field}“ gali būti tik abėcėlės raidės.',
+    'alpha_dash'          => 'Lauke „{field}“ gali būti tik raidės, skaičiai, brūkšneliai ir apatiniai brūkšneliai.',
+    'alpha_numeric'       => 'Lauke „{field}“ gali būti tik raidės ir skaičiai.',
+    'alpha_numeric_punct' => 'Lauke „{field}“ gali būti tik skaičiai, raidės, tarpeliai, ir  ~ ! # $ % & * - _ + = | : . simboliai.',
+    'alpha_numeric_space' => 'Lauke „{field}“ gali būti tik raidės, skaičiai ir tarpai.',
+    'alpha_space'         => 'Lauke „{field}“ gali būti tik raidės ir tarpai.',
+    'decimal'             => 'Lauke „{field}“ turi būti dešimtainis skaičius.',
+    'differs'             => 'Laukas „{field}“ turi skirtis nuo „{param}“ lauko.',
+    'equals'              => 'Laukas „{field}“ turi tiksliai atitikti: {param}.',
+    'exact_length'        => '{param, plural,
                                     =0 {Laukas „{field}“ turi būti tiksliai nulio ženklų ilgio.}
                                     =1 {Laukas „{field}“ turi būti tiksliai vieno ženklo ilgio.}
                                     one {Laukas „{field}“ turi būti tiksliai # ženklo ilgio.}
@@ -54,30 +54,30 @@ return [
                                     few {Lauke „{field}“ negali būti daugiau nei # ženklai}
                                     other {Lauke „{field}“ negali būti daugiau nei # ženklų}
                                 }',
-    'min_length'            => '{param, plural,
+    'min_length' => '{param, plural,
                                     =0 {Lauke „{field}“ negali būti mažiau nei nulis ženklų}
                                     =1 {Lauke „{field}“ negali būti mažiau nei vienas ženklas}
                                     one {Lauke „{field}“ negali būti mažiau nei # ženklas}
                                     few {Lauke „{field}“ negali būti mažiau nei # ženklai}
                                     other {Lauke „{field}“ negali būti mažiau nei # ženklų}
                                 }',
-    'not_equals'            => 'Lauko „{field}“ reikšmė negali būti lygi „{param}“.',
-    'not_in_list'           => 'Lauko „{field}“ reikšmės negali atitikti vienos iš šio sąrašo: „{param}“.',
-    'numeric'               => 'Lauke „{field}“ gali būti tik skaičiai.',
-    'regex_match'           => 'Lauke „{field}“ įrašyti neteisingo formato duomenys.',
-    'required'              => 'Laukas „{field}“ yra privalomas.',
-    'required_with'         => 'Laukas „{field}“ yra privalomas kai yra nustatytas „{param}“.',
-    'required_without'      => 'Laukas „{field}“ yra privalomas kai nėra nustatytas „{param}“.',
-    'string'                => 'Laukas „{field}“ turi būti eilutės (string) tipo.',
-    'timezone'              => 'Laukas „{field}“ turi atitikti egzistuojančią laiko zoną.',
-    'valid_base64'          => 'Lauke „{field}“ turi būti validi base64 eilutė.',
-    'valid_email'           => 'Lauke „{field}“ turi būti teisyklingas el. pašto adresas.',
-    'valid_emails'          => 'Lauke „{field}“ visi el. pašto adresai turi būti teisyklingi.',
-    'valid_ip'              => 'Lauke „{field}“ turi būti taisyklingas IP adresas.',
-    'valid_url'             => 'Lauke „{field}“ turi būti taisyklingas URL.',
-    'valid_url_strict'      => 'Lauke „{field}“ turi būti taisyklingas URL.',
-    'valid_date'            => 'Lauke „{field}“ turi būti taisyklinga data.',
-    'valid_json'            => 'Lauke „{field}“ turi būti taisyklingas json.',
+    'not_equals'       => 'Lauko „{field}“ reikšmė negali būti lygi „{param}“.',
+    'not_in_list'      => 'Lauko „{field}“ reikšmės negali atitikti vienos iš šio sąrašo: „{param}“.',
+    'numeric'          => 'Lauke „{field}“ gali būti tik skaičiai.',
+    'regex_match'      => 'Lauke „{field}“ įrašyti neteisingo formato duomenys.',
+    'required'         => 'Laukas „{field}“ yra privalomas.',
+    'required_with'    => 'Laukas „{field}“ yra privalomas kai yra nustatytas „{param}“.',
+    'required_without' => 'Laukas „{field}“ yra privalomas kai nėra nustatytas „{param}“.',
+    'string'           => 'Laukas „{field}“ turi būti eilutės (string) tipo.',
+    'timezone'         => 'Laukas „{field}“ turi atitikti egzistuojančią laiko zoną.',
+    'valid_base64'     => 'Lauke „{field}“ turi būti validi base64 eilutė.',
+    'valid_email'      => 'Lauke „{field}“ turi būti teisyklingas el. pašto adresas.',
+    'valid_emails'     => 'Lauke „{field}“ visi el. pašto adresai turi būti teisyklingi.',
+    'valid_ip'         => 'Lauke „{field}“ turi būti taisyklingas IP adresas.',
+    'valid_url'        => 'Lauke „{field}“ turi būti taisyklingas URL.',
+    'valid_url_strict' => 'Lauke „{field}“ turi būti taisyklingas URL.',
+    'valid_date'       => 'Lauke „{field}“ turi būti taisyklinga data.',
+    'valid_json'       => 'Lauke „{field}“ turi būti taisyklingas json.',
 
     // Credit Cards
     'valid_cc_num' => 'Nepanašu, kad lauke „{field}“ būtų įrašytas taisyklingas kredito kortelės numeris.',
