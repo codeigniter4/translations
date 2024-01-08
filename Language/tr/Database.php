c54437f180a4e9196ec3972040d277e7235a752a
Utku Aslan <138629898+aslanutku@users.noreply.github.com>
2024-01-09 02:22:07 +0300

    [tr] Update Database.php

--- Language/tr/Database.php
+++ Language/tr/Database.php
@@ -11,8 +11,21 @@
 
 // Database language settings
 return [
-    'invalidEvent'         => '{0} geçersiz Model Event geri çağrısı.',
-    'invalidArgument'      => 'Geçerli bir {0} belirtmelisiniz.',
-    'invalidAllowedFields' => 'Modelde izin verilen alanlar belirtilmeli: {0}',
-    'emptyDataset'         => '{0} içinde veri yok.',
+    'invalidEvent'                     => '"{0}" geçerli bir Model Olayı (Event) geri çağırma işlevi değil.',
+    'invalidArgument'                  => 'Geçerli bir "{0}" sağlamalısınız.',
+    'invalidAllowedFields'             => 'İzin verilen alanlar model için belirtilmeli: "{0}"',
+    'emptyDataset'                     => '{0} için veri yok.',
+    'emptyPrimaryKey'                  => '{0} yapmaya çalışırken birincil anahtar tanımlı değil.',
+    'failGetFieldData'                 => 'Veritabanından alan verileri alınamadı.',
+    'failGetIndexData'                 => 'Veritabanından indeks verileri alınamadı.',
+    'failGetForeignKeyData'            => 'Veritabanından yabancı anahtar verileri alınamadı.',
+    'parseStringFail'                  => 'Anahtar dizesi ayrıştırması başarısız oldu.',
+    'featureUnavailable'               => 'Bu özellik, kullandığınız veritabanı için mevcut değil.',
+    'tableNotFound'                    => 'Güncel veritabanında "{0}" tablosu bulunamadı.',
+    'noPrimaryKey'                     => '"{0}" model sınıfı bir Birincil Anahtar belirtmiyor.',
+    'noDateFormat'                     => '"{0}" model sınıfı geçerli bir dateFormat\'a sahip değil.',
+    'fieldNotExists'                   => '"{0}" alanı bulunamadı.',
+    'forEmptyInputGiven'               => '"{0}" alanı için boş ifade verildi.',
+    'forFindColumnHaveMultipleColumns' => 'Sütun adında yalnızca tek sütuna izin verilir.',
+    'methodNotAvailable'               => '"{0}" içinde "{1}" kullanamazsınız. Bu, Sorgu Oluşturucu (Query Builder) Sınıfının bir yöntemidir.',
 ];
