cf7a00292c42b20feaf2e44d4cb100c99bd038bd
Utku Aslan <138629898+aslanutku@users.noreply.github.com>
2024-01-13 21:29:34 +0300

    [tr] Add Security.php

---
+++ Language/tr/Security.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Security language settings
+return [
+    'disallowedAction' => 'İstediğiniz eyleme izin verilmiyor.',
+
+    // @deprecated
+    'invalidSameSite' => 'SameSite değeri None, Lax, Strict veya boş bir dize olmalıdır. Verilen: "{0}"',
+];
