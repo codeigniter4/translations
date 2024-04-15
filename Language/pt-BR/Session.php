c539870d454b99a5cfbcb95f2e762f04bd798fd5
Michael Martins <michaelfm21@gmail.com>
2024-04-14 22:16:35 -0300

    Update Session.php

--- Language/pt-BR/Session.php
+++ Language/pt-BR/Session.php
@@ -11,10 +11,12 @@
 
 // Session language settings
 return [
-    'missingDatabaseTable'   => '`sessionSavePath` deve ter o nome da tabela para o Database Session Handler funcionar.',
+    'missingDatabaseTable'   => '"sessionSavePath" deve ter o nome da tabela para o Database Session Handler funcionar.',
     'invalidSavePath'        => 'Sessão: O save path "{0}" configurado não é um diretório, não existe ou não pode ser criado.',
     'writeProtectedSavePath' => 'Sessão: O save path "{0}" configurado não é gravável pelo processo do PHP.',
     'emptySavePath'          => 'Sessão: Nenhum save path configurado.',
     'invalidSavePathFormat'  => 'Sessão: Formato do Redis save path é inválido: {0}',
+
+    // @deprecated
     'invalidSameSiteSetting' => 'Sessão: A configuração SameSite deve ser None, Lax, Strict, ou uma string vazia. Dado: {0}',
 ];
