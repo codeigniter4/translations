94ca52ca6bbe32a8e772c6ec43093f30b071ba18
Michael Martins <michaelfm21@gmail.com>
2024-04-14 21:41:57 -0300

    Update Core.php

--- Language/pt-BR/Core.php
+++ Language/pt-BR/Core.php
@@ -12,10 +12,10 @@
 // Core language settings
 return [
     'copyError'                    => 'Um erro foi encontrado ao tentar substituir o arquivo. Por favor, certifique-se de que seu diretório de arquivos é gravável.',
-    'enabledZlibOutputCompression' => 'Sua diretiva zlib.output_compression ini está on. Isso não irá trabalhar bem com a saída de buffers.',
+    'enabledZlibOutputCompression' => 'Sua diretiva zlib.output_compression ini está ligada. Isso não irá trabalhar bem com a saída de buffers.',
     'invalidFile'                  => 'Arquivo inválido: {0}',
     'invalidDirectory'             => 'O diretório não existe: "{0}"',
     'invalidPhpVersion'            => 'Sua versão do PHP deve ser {0} ou mais recente para rodar CodeIgniter. Versão atual: {1}',
     'missingExtension'             => 'A extensão {0} não está carregada.',
-    'noHandlers'                   => '{0} deve prover pelo menos um Handler.',
+    'noHandlers'                   => '{0} deve fornecedor pelo menos um Handler.',
 ];
