b0037d5d9b35e19bf10b1deedb7e0208adf916c2
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2024-04-15 21:32:10 +0800

    Do not instantiate a TestCase class

--- tests/AutoReview/TranslationsCodeTest.php
+++ tests/AutoReview/TranslationsCodeTest.php
@@ -36,12 +36,9 @@ final class TranslationsCodeTest extends TestCase
 
     public function testAllTranslatedLocalesAreIncludedInTheReadMe(): void
     {
-        $locales = (new class () extends AbstractTranslationTestCase {})->translationKeys();
-        $locales = array_keys($locales);
-
         $readme = $this->getContentsOrFail(__DIR__ . '/../../README.md');
 
-        foreach ($locales as $locale) {
+        foreach ($this->locales() as $locale) {
             $this->assertSame(1, preg_match(sprintf('/\| %s \s+\|/', $locale), $readme), sprintf(
                 'The locale "%s" is not found in the README.md file. Please add it.',
                 $locale
@@ -51,16 +48,27 @@ final class TranslationsCodeTest extends TestCase
 
     public function testAllTranslatedLocalesAreNotIncludedInMissing(): void
     {
-        $locales = (new class () extends AbstractTranslationTestCase {})->translationKeys();
-        $locales = array_keys($locales);
-
         $missing = $this->getContentsOrFail(__DIR__ . '/../../MISSING.md');
 
-        foreach ($locales as $locale) {
+        foreach ($this->locales() as $locale) {
             $this->assertSame(0, preg_match(sprintf('/\| %s \s+\|/', $locale), $missing), sprintf(
                 'The locale "%s" is already translated and should no longer be found in MISSING.md file.',
                 $locale
             ));
+        }
+    }
+
+    /**
+     * Get all the ISO 639-1 and 639-2 locale codes.
+     *
+     * @return iterable<string>
+     */
+    private function locales(): iterable
+    {
+        helper('filesystem');
+
+        foreach (directory_map(getcwd() . '/Language', 1) as $dir) {
+            yield trim($dir, '\\/');
         }
     }
 
