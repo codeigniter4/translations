4308c3b3e99f01c77099723a6eabf3c308f94cbb
Michael Martins <michaelfm21@gmail.com>
2024-04-21 17:26:09 -0300

    [pt-BR] Update Cache

--- Language/pt-BR/Cache.php
+++ Language/pt-BR/Cache.php
@@ -11,8 +11,8 @@
 
 // Cache language settings
 return [
-    'unableToWrite'   => 'O cache está incapaz de escrever em {0}',
-    'invalidHandlers' => 'A configuração de cache deve ter uma array de $validHandlers.',
-    'noBackup'        => 'A configuração de cache deve ter um handler e um backupHandler definido.',
-    'handlerNotFound' => 'A configuração de cache possui um handler ou backup handler inválido na definição.',
+    'unableToWrite'   => 'O cache está incapaz de escrever em {0}', //'Cache unable to write to "{0}".',
+    'invalidHandlers' => 'A configuração de cache deve ter uma array de $validHandlers.', //'Cache config must have an array of $validHandlers.',
+    'noBackup'        => 'A configuração de cache deve ter um handler e um backupHandler definido.', //'Cache config must have a handler and backupHandler set.',
+    'handlerNotFound' => 'A configuração de cache possui um handler ou backup handler inválido na definição.', //'Cache config has an invalid handler or backup handler specified.',
 ];
