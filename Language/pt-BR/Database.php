e7b20eb55a3a3b5169e3e508f3a5ee24a41523e6
Michael Martins <michaelfm21@gmail.com>
2024-04-21 17:55:18 -0300

    [pt-BR] Update Database

--- Language/pt-BR/Database.php
+++ Language/pt-BR/Database.php
@@ -11,21 +11,21 @@
 
 // Database language settings
 return [
-    'invalidEvent'                     => '{0} não é um callback de Model Event válido.',
-    'invalidArgument'                  => 'Você deve fornecer um {0} válido.',
-    'invalidAllowedFields'             => 'Os campos permitidos devem ser especificados para o model: {0}',
-    'emptyDataset'                     => 'Não há dados para {0}.',
-    'emptyPrimaryKey'                  => 'Não há nenhuma chave primária definida ao tentar fazer {0}.',
-    'failGetFieldData'                 => 'Falha ao obter dados de campo do banco de dados.',
-    'failGetIndexData'                 => 'Falha ao obter dados de índice do banco de dados.',
-    'failGetForeignKeyData'            => 'Falha ao obter dados de chave estrangeira do banco de dados.',
-    'parseStringFail'                  => 'Análise da chave string falhou.',
-    'featureUnavailable'               => 'Este recurso não está disponível para o banco de dados que você está usando.',
-    'tableNotFound'                    => 'Tabela `{0}` não foi encontrada no banco de dados atual.',
-    'noPrimaryKey'                     => 'Classe model `{0}` não especifica uma Chave Primária.',
-    'noDateFormat'                     => 'Classe model `{0}` não tem uma data válida.',
-    'fieldNotExists'                   => 'Campo `{0}` não encontrado.',
-    'forEmptyInputGiven'               => 'Declaração vazia passada para o campo `{0}`',
+    'invalidEvent'                     => '"{0}" não é um callback de Model Event válido.', // '"{0}" is not a valid Model Event callback.',
+    'invalidArgument'                  => 'Você deve fornecer um "{0}" válido.', // 'You must provide a valid "{0}".',
+    'invalidAllowedFields'             => 'Os campos permitidos devem ser especificados para o model: "{0}"', // 'Allowed fields must be specified for model: "{0}"',
+    'emptyDataset'                     => 'Não há dados para "{0}".', // 'There is no data to {0}.',
+    'emptyPrimaryKey'                  => 'Não há nenhuma chave primária definida ao tentar fazer "{0}".', // 'There is no primary key defined when trying to make {0}.',
+    'failGetFieldData'                 => 'Falha ao obter dados de campo do banco de dados.', // 'Failed to get field data from database.',
+    'failGetIndexData'                 => 'Falha ao obter dados de índice do banco de dados.', // 'Failed to get index data from database.',
+    'failGetForeignKeyData'            => 'Falha ao obter dados de chave estrangeira do banco de dados.', // 'Failed to get foreign key data from database.',
+    'parseStringFail'                  => 'Análise da chave string falhou.', // 'Parsing key string failed.',
+    'featureUnavailable'               => 'Este recurso não está disponível para o banco de dados que você está usando.', // 'This feature is not available for the database you are using.',
+    'tableNotFound'                    => 'Tabela "{0}" não foi encontrada no banco de dados atual.', // 'Table "{0}" was not found in the current database.',
+    'noPrimaryKey'                     => 'Classe model "{0}" não especifica uma Chave Primária.', // '"{0}" model class does not specify a Primary Key.',
+    'noDateFormat'                     => 'Classe model "{0}" não tem uma data válida.', // '"{0}" model class does not have a valid dateFormat.'
+    'fieldNotExists'                   => 'Campo `{0}` não encontrado.', // 'Field "{0}" not found.',
+    'forEmptyInputGiven'               => 'Declaração vazia passada para o campo "{0}"', // 'Empty statement is given for the field "{0}"'
     'forFindColumnHaveMultipleColumns' => 'Apenas uma coluna é permitida no nome da coluna.',
-    'methodNotAvailable'               => 'Você não pode usar `{1}` em `{0}`. Este é um método da classe `Query Builder`.',
+    'methodNotAvailable'               => 'Você não pode usar "{1}" em "{0}". Este é um método da classe `Query Builder`.',
 ];
