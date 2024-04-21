4afe7d471dbf4fea190854f6124ac526028c68ae
Michael Martins <michaelfm21@gmail.com>
2024-04-21 18:58:48 -0300

    [pt-BR] Update Publisher

--- Language/pt-BR/Publisher.php
+++ Language/pt-BR/Publisher.php
@@ -11,12 +11,12 @@
 
 // Publisher language settings
 return [
-    'collision'             => 'Publisher encontrou uma {0} inesperada enquanto copiava {1} para {2}.',
-    'destinationNotAllowed' => 'Destino não está na lista de diretórios permitidos do Publisher: {0}',
-    'fileNotAllowed'        => '{0} falha a seguinte restrição para {1}: {2}',
+    'collision'             => 'Publisher encontrou uma "{0}" inesperada enquanto copiava "{1}" para "{2}".', // 'Publisher encountered an unexpected "{0}" while copying "{1}" to "{2}".',
+    'destinationNotAllowed' => 'Destino não está na lista de diretórios permitidos do Publisher: "{0}"', // 'Destination is not on the allowed list of Publisher directories: "{0}"',
+    'fileNotAllowed'        => '"{0}" falha a seguinte restrição para "{1}": {2}', // '"{0}" fails the following restriction for "{1}": {2}',
 
     // Publish Command
-    'publishMissing' => 'Nenhuma classe Publisher detectada em {0} através de todos os namespaces.',
-    'publishSuccess' => '{0} publicou {1} arquivo(s) para {2}.',
-    'publishFailure' => '{0} falhou para publicar para {1}!',
+    'publishMissing' => 'Nenhuma classe Publisher detectada em {0} através de todos os namespaces.', // 'No Publisher classes detected in {0} across all namespaces.',
+    'publishSuccess' => '"{0}" publicou {1} arquivo(s) para "{2}".', // '"{0}" published {1} file(s) to "{2}".',
+    'publishFailure' => '"{0}" falhou ao publicar para "{1}".', // '"{0}" failed to publish to "{1}".',
 ];
