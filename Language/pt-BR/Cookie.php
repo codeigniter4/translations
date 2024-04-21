3f89d449a6ebf64c08325b3bf7bc9c2a474563a9
Michael Martins <michaelfm21@gmail.com>
2024-04-21 17:58:21 -0300

    [pt-BR] Update Cookie

--- Language/pt-BR/Cookie.php
+++ Language/pt-BR/Cookie.php
@@ -17,8 +17,8 @@ return [
     'emptyCookieName'       => 'O nome do cookie não pode ser vazio.', // 'The cookie name cannot be empty.',
     'invalidSecurePrefix'   => 'Usando o prefixo "__Secure-" requer que seja definido o atributo "Secure".', // 'Using the "__Secure-" prefix requires setting the "Secure" attribute.',
     'invalidHostPrefix'     => 'Usando o prefixo "__Host-" deve ser definido com a flag "Secure", não deve possuir o atributo "Domain", e "Path" deve ser "/".', // 'Using the "__Host-" prefix must be set with the "Secure" flag, must not have a "Domain" attribute, and the "Path" is set to "/".',
-    'invalidSameSite'       => 'O valor de SameSite deve ser None, Lax, Strict ou uma string vazia, {0} foi passado.', // 'The SameSite value must be None, Lax, Strict or a blank string, {0} given.',
+    'invalidSameSite'       => 'O valor de SameSite deve ser None, Lax, Strict ou uma string vazia, "{0}" foi passado.', // 'The SameSite value must be None, Lax, Strict or a blank string, {0} given.',
     'invalidSameSiteNone'   => 'Usando o atributo "SameSite=None" requer que seja definido o atributo "Secure".', // 'Using the "SameSite=None" attribute requires setting the "Secure" attribute.',
-    'invalidCookieInstance' => 'Classe "{0}" espera array de cookies serem instâncias de "{1}" mas recebeu "{2}" no índice {3}.', // '"{0}" class expected cookies array to be instances of "{1}" but got "{2}" at index {3}.',
+    'invalidCookieInstance' => 'Classe "{0}" espera array de cookies serem instâncias de "{1}" mas recebeu "{2}" no índice "{3}".', // '"{0}" class expected cookies array to be instances of "{1}" but got "{2}" at index {3}.',
     'unknownCookieInstance' => 'Objeto Cookie com o nome "{0}" e prefixo "{1}" não foi encontrado na coleção.', // 'Cookie object with name "{0}" and prefix "{1}" was not found in the collection.',
 ];
