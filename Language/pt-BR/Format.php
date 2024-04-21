851cdad7bfdbb28508675e1b2ef42b3ad7f5bfe7
Michael Martins <michaelfm21@gmail.com>
2024-04-21 18:11:50 -0300

    [pt-BR] Update Format

--- Language/pt-BR/Format.php
+++ Language/pt-BR/Format.php
@@ -11,8 +11,8 @@
 
 // Format language settings
 return [
-    'invalidFormatter' => '"{0}" não é uma classe Formatter válida.',
-    'invalidJSON'      => 'Falha ao analisar a string json, erro: "{0}".',
-    'invalidMime'      => 'Nenhum Formatter definido para o tipo mime: "{0}".',
-    'missingExtension' => 'A extensão SimpleXML é necessária para formatar XML.',
+    'invalidFormatter' => '"{0}" não é uma classe Formatter válida.', // '"{0}" is not a valid Formatter class.',
+    'invalidJSON'      => 'Falha ao analisar a string JSON. Erro: "{0}".', // 'Failed to parse JSON string. Error: {0}',
+    'invalidMime'      => 'Nenhum Formatter definido para o tipo mime: "{0}".', // 'No Formatter defined for mime type: "{0}".',
+    'missingExtension' => 'A extensão SimpleXML é necessária para formatar XML.', // 'The SimpleXML extension is required to format XML.',
 ];
