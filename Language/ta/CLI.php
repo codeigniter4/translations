162228e29da22e39ff897a6d32e684447990362a
Leslin <leslin@Leslins-MacBook-Air.local>
2024-05-16 16:46:07 +0530

    incompleted tamil lang support for CLI

---
+++ Language/ta/CLI.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// CLI language settings
+return [
+    'altCommandPlural'   => 'இவற்றில் ஒன்றையா நீங்கள் குறிப்பிடுகிறீர்கள்?',
+    'altCommandSingular' => 'நீங்கள் இதை சொன்னீர்களா?',
+    'commandNotFound'    => 'கட்டளை "{0}" காணப்படவில்லை.',
+    'generator'          => [
+        'cancelOperation' => 'செயல்பாடு ரத்து செய்யப்பட்டுள்ளது.',
+        'className'       => [
+            'cell'       => 'செல் வகுப்பின் பெயர்',
+            'command'    => 'கட்டளை வகுப்பின் பெயர்',
+            'config'     => 'கட்டமைப்பு வகுப்பின் பெயர்',
+            'controller' => 'கட்டுப்படுத்தி வகுப்பின் பெயர்',
+            'default'    => 'வகுப்பின் பெயர்',
+            'entity'     => 'நிறுவன வகுப்பின் பெயர்',
+            'filter'     => 'வடிகட்டி வகுப்பின் பெயர்',
+            'migration'  => 'இடம்பெயர்வு வகுப்பின் பெயர்',
+            'model'      => 'மாதிரி வகுப்பின் பெயர்',
+            'seeder'     => 'விதையமைவு வகுப்பின் பெயர்',
+            'test'       => 'சோதனை வகுப்பின் பெயர்',
+            'validation' => 'சரிபார்த்தல் வகுப்பின் பெயர்',
+        ],
+        'commandType'      => 'கட்டளை வகை',
+        'databaseGroup'    => 'தரவுத்தள குழு',
+        'fileCreate'       => 'கோப்பு உருவாக்கப்பட்டது: {0}',
+        'fileError'        => 'கோப்பு உருவாக்கத்தில் பிழை: "{0}"',
+        'fileExist'        => 'கோப்பு உள்ளது: "{0}"',
+        'fileOverwrite'    => 'கோப்பு மேலெழுதப்பட்டது: "{0}"',
+        'parentClass'      => 'பெற்றோர் வகுப்பு',
+        'returnType'       => 'Return type',
+        'tableName'        => 'Table name',
+        'usingCINamespace' => 'Warning: Using the "CodeIgniter" namespace will generate the file in the system directory.',
+        'viewName'         => [
+            'cell' => 'Cell view name',
+        ],
+    ],
+    'helpArguments'       => 'Arguments:',
+    'helpDescription'     => 'Description:',
+    'helpOptions'         => 'Options:',
+    'helpUsage'           => 'Usage:',
+    'invalidColor'        => 'Invalid "{0}" color: "{1}".',
+    'namespaceNotDefined' => 'Namespace "{0}" is not defined.',
+];
