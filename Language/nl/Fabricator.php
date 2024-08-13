c8ac0380ebf9703d2f003dcda35af81e82fcc680
christianberkman <39840601+christianberkman@users.noreply.github.com>
2024-08-13 09:26:35 +0300

    Create Fabricator.php

---
+++ Language/nl/Fabricator.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Fabricator language settings
+return [
+    'invalidModel'      => 'Onjuist model gegeven for fabricatie.',
+    'missingFormatters' => 'Er zijn geen valide Formatters dedefineerd.',
+    'createFailed'      => 'Fabricator kon niet invoegen in de tabel "{0}": {1}',
+];
