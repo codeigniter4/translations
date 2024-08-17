5e196f16991c554b17462cc9926582a8e37bcd4e
Christian Berkman <christianberkman@gmail.com>
2024-08-17 15:52:43 +0300

    fix PR issues

--- Language/nl/Cookie.php
+++ Language/nl/Cookie.php
@@ -18,7 +18,7 @@ return [
     'invalidCookieName'     => 'De cookie-naam "{0}" bevat tekens die niet zijn toegestaan.',
     'emptyCookieName'       => 'De cookie-naam mag niet leeg zijn.',
     'invalidSecurePrefix'   => 'Om de prefix "__Secure-" te gebruiken moet het "Secure" attribuut ingesteld zijn.',
-    'invalidHostPrefix'     => 'Using the "__Host-" prefix must be set with the "Secure" flag, must not have a "Domain" attribute, and the "Path" is set to "/".',
+    'invalidHostPrefix'     => 'De "__Host-" prefix gebruikt worden met de "Secure" optie, mag geen "Domain" optie hebben, en "Path" moet "/" zijn.',
     'invalidSameSite'       => 'De waarde van SameSite moet zijn: None, Lax, Strict of een lege string, {0} gegeven.',
     'invalidSameSiteNone'   => 'Om "SameSite=None" te gebruiken moet het "Secure" attribuut ingesteld zijn.',
     'invalidCookieInstance' => '"{0}" class verwacht dat de cookie-array in instance is van "{1}" maar vond "{2}" voor index {3}.',
