c0378e6f773a43f0b0a69c18e56c655f53af66f1
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2025-01-26 14:59:56 +0800

    style: fix coding styles (#475)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -42,5 +42,5 @@ $options = [
 return Factory::create(new CodeIgniter4(), $overrides, $options)->forLibrary(
     'CodeIgniter 4 framework',
     'CodeIgniter Foundation',
-    'admin@codeigniter.com'
+    'admin@codeigniter.com',
 );
