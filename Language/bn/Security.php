a6b67c17816fa509ce84372f94631a34612d0810
Ariful Alam <ariful-alam@hotmail.com>
2025-08-02 18:26:25 +0600

    [bn] add missing bengali language translations (#499)
    * missing bengali language translations
    
    * text check
    
    * CLI.generator.className.test added
    
    * composer cs-fix

--- Language/bn/Security.php
+++ Language/bn/Security.php
@@ -12,6 +12,7 @@
 // Security language settings
 return [
     'disallowedAction' => 'আপনি যে কর্মের অনুরোধ করেছেন তা অনুমোদিত নয়।',
+    'insecureCookie'   => 'অসুরক্ষিত সংযোগে সুরক্ষিত কুকি প্রেরণ করা হয়েছে।', // 'Attempted to send a secure cookie over a non-secure connection.'
 
     // @deprecated
     'invalidSameSite' => 'SameSite মানটি অবশ্যই None, Lax, Strict, বা একটি ফাঁকা স্ট্রিং হতে হবে। দেওয়া হয়েছে: {0}',
