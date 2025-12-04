45de29c4fdc17a22c2351a40e095846b8c337d1e
Terrorboy <z9n@kakao.com>
2025-12-04 13:17:59 +0900

    [ko] add Honeypot (#506)

---
+++ Language/ko/Honeypot.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Honeypot language settings
+return [
+    'noTemplate'      => 'Honeypot의 HTML 템플릿이 구성되지 않았습니다.', // 'The HTML template for the Honeypot is not configured.'
+    'noNameField'     => 'Honeypot 필드의 이름이 설정되지 않았습니다.', // 'The name of the Honeypot field is not set.'
+    'theClientIsABot' => 'Honeypot 클라이언트는 봇일 수 있습니다.', // 'The Honeypot client may be a bot.'
+];
