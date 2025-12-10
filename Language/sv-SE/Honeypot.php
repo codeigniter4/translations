993a45bcf1207ff6c091a318ed2a2b9fe20f2602
Mattias Sandström <1013635+tangix@users.noreply.github.com>
2025-12-10 11:26:04 +0100

    [sv-SE] Added file for Honeypot. (#511)

---
+++ Language/sv-SE/Honeypot.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'noTemplate'      => 'HTML-mallen för Honeypotfunktionen är inte konfigurerad.',
+    'noNameField'     => 'Namnet på Honeypotfältet är inte angivet.',
+    'theClientIsABot' => 'Honeypotklienten kan vara en bot.',
+];
