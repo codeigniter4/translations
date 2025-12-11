43188c1d86365be164917de07da5be81c5ae28cf
patel-vansh <developer.patelvansh@gmail.com>
2025-12-11 09:52:21 +0530

    fix: Add missing Honeypot

---
+++ Language/hi/Honeypot.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Honeypot language settings
+return [
+    'noTemplate'      => 'Honeypot HTML टेम्प्लेट कॉन्फ़िगर नहीं किया गया है।', // 'The HTML template for the Honeypot is not configured.'
+    'noNameField'     => 'Honeypot फ़ील्ड का नाम सेट नहीं है।', // 'The name of the Honeypot field is not set.'
+    'theClientIsABot' => 'संभावना है कि अनुरोधकर्ता एक बॉट है।', // 'The Honeypot client may be a bot.'
+];
