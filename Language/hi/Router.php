9f7888ca483f6350c4a0d402d60d2aa4a0fa734f
Vansh Patel <102534216+patel-vansh@users.noreply.github.com>
2025-12-12 16:28:16 +0530

    [hi] fix: Add missing Honeypot (#513)

--- Language/hi/Router.php
+++ Language/hi/Router.php
@@ -16,5 +16,5 @@ return [
     'invalidParameter'         => 'एक पैरामीटर अपेक्षित प्रकार से मेल नहीं खाता।', // 'A parameter does not match the expected type.'
     'missingDefaultRoute'      => 'यह निर्धारित करने में असमर्थ कि क्या प्रदर्शित किया जाना चाहिए। रूटिंग फ़ाइल में एक डिफ़ॉल्ट रूट निर्दिष्ट नहीं किया गया है।', // 'Unable to determine what should be displayed. A default route has not been specified in the routing file.'
     'invalidDynamicController' => 'सुरक्षा कारणों से एक गतिशील कंट्रोलर की अनुमति नहीं है। रूट हैंडलर: "{0}"', // 'A dynamic controller is not allowed for security reasons. Route handler: "{0}"'
-    'invalidControllerName'    => 'नेमस्पेस डीलिमिटर एक बैकस्लैश (\) है, न कि एक स्लैश (/)। रूट हैंडलर: "{0}"', // 'The namespace delimiter is a backslash (\), not a slash (/). Route handler: "{0}"'
+    'invalidControllerName'    => 'नेमस्पेस डीलिमिटर एक बैकस्लैश (\) है, न कि एक स्लैश (/)। रूट हैंडलर: "{0}"', // 'The namespace delimiter is a backslash (\\), not a slash (/). Route handler: "{0}"'
 ];
