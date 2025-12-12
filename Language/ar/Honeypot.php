50a84c7073d482769d5a788c48adcfdaa0c4db65
Mohamed Alosman <46384940+aboosamah94@users.noreply.github.com>
2025-12-12 23:24:05 +0300

    Add missing to ar
    - Honeypot.php

---
+++ Language/ar/Honeypot.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Honeypot language settings
+return [
+    'noTemplate'      => 'لم يتم إعداد قالب HTML الخاص بـ Honeypot.',
+    'noNameField'     => 'لم يتم تعيين اسم حقل Honeypot.',
+    'theClientIsABot' => 'قد يكون عميل Honeypot روبوتًا (bot).',
+];
