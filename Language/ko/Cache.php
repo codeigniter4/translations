ef51e90a06f37c090a4bd013f40fc1ea6d1c6e28
Terrorboy <z9n@kakao.com>
2026-04-22 10:41:22 +0900

    [ko] Update Cache

--- Language/ko/Cache.php
+++ Language/ko/Cache.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 // Cache language settings
 return [
     'unableToWrite'   => '"{0}"에 캐시를 쓸 수 없습니다.', // 'Cache unable to write to "{0}".'
+    'invalidHandler'  => '캐시 드라이버 "{0}"은 유효한 캐시 핸들러가 아닙니다.', // 'Cache driver "{0}" is not a valid cache handler.'
     'invalidHandlers' => '캐시 구성에는 $validHandlers 배열이 있어야 합니다.', // 'Cache config must have an array of $validHandlers.'
     'noBackup'        => '캐시 구성에는 핸들러와 백업 핸들러 세트가 설정되어야 합니다.', // 'Cache config must have a handler and backupHandler set.'
     'handlerNotFound' => '캐시 구성에 잘못된 핸들러 또는 백업 핸들러가 지정되어 있습니다.', // 'Cache config has an invalid handler or backup handler specified.'
