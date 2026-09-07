97a612d48338f33d0fb0dfd10fc067ea2139ffc0
Terrorboy <z9n@kakao.com>
2026-09-07 15:49:34 +0900

    [ko] Update Cookie translations

--- Language/ko/Cookie.php
+++ Language/ko/Cookie.php
@@ -15,7 +15,8 @@ declare(strict_types=1);
 return [
     'invalidExpiresTime'    => '"만료" 속성에 대한 "{0}" 유형이 잘못되었습니다. 예상 : 문자열, 정수, DateTimeInterface object.', // 'Invalid "{0}" type for "Expires" attribute. Expected: string, integer, DateTimeInterface object.'
     'invalidExpiresValue'   => '쿠키 만료 시간이 유효하지 않습니다.', // 'The cookie expiration time is not valid.'
-    'invalidCookieName'     => '쿠키 이름 "{0}"에 잘못된 문자가 있습니다.', // 'The cookie name "{0}" contains invalid characters.'
+    'invalidCookieName'     => '쿠키 이름 "{0}"에 잘못된 문자가 포함되어 있습니다.', // 'The cookie name "{0}" contains invalid characters.'
+    'invalidCookieValue'    => '쿠키 값에 잘못된 문자가 포함되어 있습니다.', // 'The cookie value contains invalid characters.'
     'emptyCookieName'       => '쿠키 이름은 비워 둘 수 없습니다.', // 'The cookie name cannot be empty.'
     'invalidSecurePrefix'   => '"__Secure-" 접두사를 사용하려면 "Secure" 속성을 설정해야합니다.', // 'Using the "__Secure-" prefix requires setting the "Secure" attribute.'
     'invalidHostPrefix'     => '"__Host-" 접두사를 사용하는 경우 "Secure" 플래그로 설정해야하며 "Domain" 속성이 없어야하며 "Path"가 "/"로 설정되어 있어야합니다.', // 'Using the "__Host-" prefix must be set with the "Secure" flag, must not have a "Domain" attribute, and the "Path" is set to "/".'
